3686d819b2baa559cc9aa2b71be583f09ddb2f94
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-10-20 19:07:46 +0100

    Moved inversions module to operators module of autoarray

--- autoarray/plotters/__init__.py
+++ autoarray/plotters/__init__.py
@@ -4,3 +4,5 @@ from .line_yx_plotters import plot_line as line
 from autoarray.plotters import imaging_plotters as imaging
 from autoarray.plotters import interferometer_plotters as interferometer
 from autoarray.plotters import fit_plotters as fit
+from autoarray.plotters import mapper_plotters as mapper
+from autoarray.plotters import inversion_plotters as inversion
--- autoarray/util/__init__.py
+++ autoarray/util/__init__.py
@@ -4,3 +4,7 @@ from ..util import fit_util as fit
 from ..util import grid_util as grid
 from ..util import mask_util as mask
 from ..util import sparse_util as sparse
+from ..util import pixelization_util as pixelization
+from ..util import regularization_util as regularization
+from ..util import mapper_util as mapper
+from ..util import inversion_util as inversion
