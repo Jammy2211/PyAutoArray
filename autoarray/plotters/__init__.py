7507461d8e71ba0ad7ce05f2ff0feacade725705
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-10-27 11:49:25 +0000

    combined different fit plotters into imaging_fit_plotter module with all tests now passing.

--- autoarray/plotters/__init__.py
+++ autoarray/plotters/__init__.py
@@ -4,6 +4,6 @@ from .line_yx_plotters import plot_line as line
 from .quantity_radii_plotters import plot_quantity_as_function_of_radius as quantity_as_function_of_radius
 from autoarray.plotters import imaging_plotters as imaging
 from autoarray.plotters import interferometer_plotters as interferometer
-from autoarray.plotters import fit_plotters as fit
+from autoarray.plotters import imaging_fit_plotters as imaging_fit
 from autoarray.plotters import mapper_plotters as mapper
 from autoarray.plotters import inversion_plotters as inversion
