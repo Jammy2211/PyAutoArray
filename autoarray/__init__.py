d223834df02d112d4784b85ea4026d63cd3d5b84
Richard <richardhayes777@gmail.com>
2019-11-08 16:00:28 +0000

    imports

--- autoarray/__init__.py
+++ autoarray/__init__.py
@@ -1,4 +1,19 @@
+from autoarray import conf
+from autoarray import exc
+from autoarray import masked
+from autoarray import plotters as plot
+from autoarray import simulator
+from autoarray import util
+from autoarray.dataset import data_converter
+from autoarray.dataset.imaging import Imaging as imaging
+from autoarray.dataset.interferometer import Interferometer as interferometer
+from autoarray.fit.fit import fit
 from autoarray.mask.mask import Mask as mask
+from autoarray.operators.convolver import Convolver as convolver
+from autoarray.operators.inversion import pixelizations as pix, regularization as reg
+from autoarray.operators.inversion.inversions import inversion
+from autoarray.operators.inversion.mappers import mapper
+from autoarray.operators.transformer import Transformer as transformer
 from autoarray.structures.arrays import Array as array
 from autoarray.structures.grids import (
     Grid as grid,
@@ -9,19 +24,5 @@ from autoarray.structures.grids import (
 )
 from autoarray.structures.kernel import Kernel as kernel
 from autoarray.structures.visibilities import Visibilities as visibilities
-from autoarray.dataset.imaging import Imaging as imaging
-from autoarray.dataset.interferometer import Interferometer as interferometer
-from autoarray.dataset import data_converter
-from autoarray.operators.convolver import Convolver as convolver
-from autoarray.operators.transformer import Transformer as transformer
-from autoarray.operators.inversion.mappers import mapper
-from autoarray.operators.inversion.inversions import inversion
-from autoarray.operators.inversion import pixelizations as pix, regularization as reg
-from autoarray import masked
-from autoarray import simulator
-from autoarray import conf
-from autoarray import plotters as plot
-from autoarray import util
-from autoarray.fit.fit import fit
 
 __version__ = "0.2.2"
