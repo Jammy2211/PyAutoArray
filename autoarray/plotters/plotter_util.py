5fb8793b80af8363a77e0a74c13bea6295a76dcf
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-11-13 21:14:54 +0000

    Updated all autolens workspace / pipelines / howtolens to new autofit API

--- autoarray/plotters/plotter_util.py
+++ autoarray/plotters/plotter_util.py
@@ -231,9 +231,7 @@ def quantity_and_annuli_radii_from_minimum_and_maximum_radii_and_radii_points(
     return quantity_radii, annuli_radii
 
 
-def get_critical_curve_and_caustic(
-    obj, include_critical_curves, include_caustics
-):
+def get_critical_curve_and_caustic(obj, include_critical_curves, include_caustics):
 
     if include_critical_curves:
         critical_curves = obj.critical_curves
