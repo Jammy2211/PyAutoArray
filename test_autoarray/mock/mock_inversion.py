44a26eee468623f638d23c213adf07b02af1b078
jammy2211 <james.w.nightingale@durham.ac.uk>
2019-12-11 14:53:13 +0000

    Cleaned up mock objects

--- test_autoarray/mock/mock_inversion.py
+++ test_autoarray/mock/mock_inversion.py
@@ -1,5 +1,17 @@
 import numpy as np
 
+class MockFitInversion(object):
+    def __init__(
+        self,
+        regularization_term,
+        log_det_curvature_reg_matrix_term,
+        log_det_regularization_matrix_term,
+    ):
+
+        self.regularization_term = regularization_term
+        self.log_det_curvature_reg_matrix_term = log_det_curvature_reg_matrix_term
+        self.log_det_regularization_matrix_term = log_det_regularization_matrix_term
+
 
 class MockPixelization(object):
     def __init__(self, value, grid=None):
