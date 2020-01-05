eccfeddb29f5e4f4e26c70177df926cb4c198c10
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-01-05 17:43:39 +0000

    Plotters for grids, lines made.

--- autoarray/plotters/line_yx_plotters.py
+++ autoarray/plotters/line_yx_plotters.py
@@ -4,180 +4,205 @@ from autoarray.util import plotter_util
 import matplotlib.pyplot as plt
 import numpy as np
 
+from autoarray.plotters import plotters
 
-def plot_line(
-    y,
-    x,
-    as_subplot=False,
-    label=None,
-    plot_axis_type="semilogy",
-    vertical_lines=None,
-    vertical_line_labels=None,
-    unit_label_y="Quantity",
-    unit_label_x="scaled",
-    unit_conversion_factor=None,
-    figsize=(7, 7),
-    plot_legend=False,
-    title="Quantity vs Radius",
-    titlesize=16,
-    xlabelsize=16,
-    ylabelsize=16,
-    xyticksize=16,
-    pointsize=20,
-    legend_fontsize=12,
-    output_path=None,
-    output_format="show",
-    output_filename="quantity_vs_radius",
-):
-
-    plotter_util.setup_figure(figsize=figsize, as_subplot=as_subplot)
-    plotter_util.set_title(title=title, titlesize=titlesize)
-
-    if y is None:
-        return
-
-    if x is None:
-        x = np.arange(len(y))
-
-    plot_y_vs_x(
-        y=y, x=x, plot_axis_type=plot_axis_type, label=label, pointsize=pointsize
-    )
-
-    set_xy_labels_and_ticksize(
-        unit_label_y=unit_label_y,
-        unit_label_x=unit_label_x,
-        ylabelsize=ylabelsize,
-        xlabelsize=xlabelsize,
-        xyticksize=xyticksize,
-    )
-
-    plot_vertical_lines(
-        vertical_lines=vertical_lines,
-        vertical_line_labels=vertical_line_labels,
-        unit_conversion_factor=unit_conversion_factor,
-    )
-
-    set_legend(plot_legend=plot_legend, legend_fontsize=legend_fontsize)
-
-    set_xticks(
-        extent=[np.min(x), np.max(x)],
-        unit_conversion_factor=unit_conversion_factor,
-        xticks_manual=None,
-    )
-
-    if output_format is not "fits":
-
-        plotter_util.output_figure(
-            array=None,
-            as_subplot=as_subplot,
+class GridPlotter(plotters.Plotter):
+    def __init__(
+            self,
+            is_sub_plotter=False,
+            use_scaled_units=None,
+            unit_conversion_factor=None,
+            include_legend=True,
+            legend_fontsize=12,
+            figsize=None,
+            aspect=None,
+            titlesize=None,
+            xlabelsize=None,
+            ylabelsize=None,
+            xyticksize=None,
+            pointsize=5,
+            label_title=None,
+            label_yunits=None,
+            label_xunits=None,
+            label_yticks=None,
+            label_xticks=None,
+            output_path=None,
+            output_format="show",
+            output_filename=None,
+    ):
+        super(GridPlotter, self).__init__(
+            is_sub_plotter=is_sub_plotter,
+            use_scaled_units=use_scaled_units,
+            unit_conversion_factor=unit_conversion_factor,
+            figsize=figsize,
+            aspect=aspect,
+            titlesize=titlesize,
+            xlabelsize=xlabelsize,
+            ylabelsize=ylabelsize,
+            xyticksize=xyticksize,
+            label_title=label_title,
+            label_yunits=label_yunits,
+            label_xunits=label_xunits,
+            label_yticks=label_yticks,
+            label_xticks=label_xticks,
             output_path=output_path,
-            output_filename=output_filename,
             output_format=output_format,
+            output_filename=output_filename,
         )
 
-    plotter_util.close_figure(as_subplot=as_subplot)
+        self.pointsize = pointsize
+        self.include_legend = include_legend
+        self.legend_fontsize = legend_fontsize
 
+    def plot_line(
+        self,
+        y,
+        x,
+        label=None,
+        plot_axis_type="semilogy",
+        vertical_lines=None,
+        vertical_line_labels=None,
+        output_path=None,
+        output_format="show",
+        output_filename="quantity_vs_radius",
+    ):
 
-def plot_y_vs_x(y, x, plot_axis_type, label, pointsize):
+        if y is None:
+            return
 
-    if plot_axis_type is "linear":
-        plt.plot(x, y, label=label)
-    elif plot_axis_type is "semilogy":
-        plt.semilogy(x, y, label=label)
-    elif plot_axis_type is "loglog":
-        plt.loglog(x, y, label=label)
-    elif plot_axis_type is "scatter":
-        plt.scatter(x, y, label=label, s=pointsize)
-    else:
-        raise exc.PlottingException(
-            "The plot_axis_type supplied to the plotter is not a valid string (must be linear "
-            "| semilogy | loglog)"
+        self.setup_figure()
+        self.set_title()
+
+        if x is None:
+            x = np.arange(len(y))
+
+        self.plot_y_vs_x(
+            y=y, x=x, plot_axis_type=plot_axis_type, label=label,
         )
 
-
-def set_xy_labels_and_ticksize(
-    unit_label_x, unit_label_y, xlabelsize, ylabelsize, xyticksize
-):
-    """Set the x and y labels of the figure, and set the fontsize of those labels.
-
-    The x label is always the distance scale / radius, thus the x-label is either arc-seconds or kpc and depending \
-    on the unit_label the figure is plotted in.
-
-    The ylabel is the physical quantity being plotted and is passed as an input parameter.
-
-    Parameters
-    -----------
-    unit_label_x : str
-        The unit_label of the y / x axis of the plots.
-    unit_conversion_factor : float
-        The conversion factor between arc-seconds and kiloparsecs, required to plotters the unit_label in kpc.
-    unit_label_y : str
-        The y-label of the figure, which is the physical quantity being plotted.
-    xlabelsize : int
-        The fontsize of the x axes label.
-    ylabelsize : int
-        The fontsize of the y axes label.
-    xyticksize : int
-        The font size of the x and y ticks on the figure axes.
-    """
-
-    plt.ylabel(ylabel=unit_label_y, fontsize=ylabelsize)
-    plt.xlabel("x (" + unit_label_x + ")", fontsize=xlabelsize)
-    plt.tick_params(labelsize=xyticksize)
-
-
-def set_xticks(extent, unit_conversion_factor, xticks_manual):
-    """Get the extent of the dimensions of the array in the unit_label of the figure (e.g. arc-seconds or kpc).
-
-    This is used to set the extent of the array and thus the y / x axis limits.
-
-    Parameters
-    -----------
-    array : data_type.array.aa.Scaled
-        The 2D array of data_type which is plotted.
-    unit_label : str
-        The label for the unit_label of the y / x axis of the plots.
-    unit_conversion_factor : float
-        The conversion factor between arc-seconds and kiloparsecs, required to plotters the unit_label in kpc.
-    xticks_manual :  [] or None
-        If input, the xticks do not use the array's default xticks but instead overwrite them as these values.
-    yticks_manual :  [] or None
-        If input, the yticks do not use the array's default yticks but instead overwrite them as these values.
-    """
-
-    xticks = np.round(np.linspace(extent[0], extent[1], 5), 2)
-
-    if xticks_manual is not None:
-        xtick_labels = np.asarray([xticks_manual[0], xticks_manual[3]])
-    elif unit_conversion_factor is None:
-        xtick_labels = np.round(np.linspace(extent[0], extent[1], 5), 2)
-    elif unit_conversion_factor is not None:
-        xtick_labels = (
-            np.round(np.linspace(extent[0], extent[1], 5), 2) * unit_conversion_factor
-        )
-    else:
-        raise exc.PlottingException(
-            "The y and y ticks cannot be set using the input options."
+        self.set_xy_labels_and_ticksize(
         )
 
-    plt.xticks(ticks=xticks, labels=xtick_labels)
+        self.plot_vertical_lines(
+            vertical_lines=vertical_lines,
+            vertical_line_labels=vertical_line_labels,
+        )
+
+        self.set_legend()
+
+        self.set_xticks(
+            extent=[np.min(x), np.max(x)],
+        )
+
+        if output_format is not "fits":
+
+            self.output_figure(
+                array=None,
+            )
+
+        self.close_figure()
 
 
-def plot_vertical_lines(vertical_lines, vertical_line_labels, unit_conversion_factor):
+    def plot_y_vs_x(self, y, x, plot_axis_type, label):
 
-    if vertical_lines is [] or vertical_lines is None:
-        return
+        if plot_axis_type is "linear":
+            plt.plot(x, y, label=label)
+        elif plot_axis_type is "semilogy":
+            plt.semilogy(x, y, label=label)
+        elif plot_axis_type is "loglog":
+            plt.loglog(x, y, label=label)
+        elif plot_axis_type is "scatter":
+            plt.scatter(x, y, label=label, s=self.pointsize)
+        else:
+            raise exc.PlottingException(
+                "The plot_axis_type supplied to the plotter is not a valid string (must be linear "
+                "| semilogy | loglog)"
+            )
 
-    for vertical_line, vertical_line_label in zip(vertical_lines, vertical_line_labels):
 
-        if unit_conversion_factor is None:
-            x_value_plot = vertical_line
-        elif unit_conversion_factor is not None:
-            x_value_plot = vertical_line * unit_conversion_factor
+    def set_xy_labels_and_ticksize(
+        self,
+    ):
+        """Set the x and y labels of the figure, and set the fontsize of those labels.
 
-        plt.axvline(x=x_value_plot, label=vertical_line_label, linestyle="--")
+        The x label is always the distance scale / radius, thus the x-label is either arc-seconds or kpc and depending \
+        on the unit_label the figure is plotted in.
+
+        The ylabel is the physical quantity being plotted and is passed as an input parameter.
+
+        Parameters
+        -----------
+        unit_label_x : str
+            The unit_label of the y / x axis of the plots.
+        unit_conversion_factor : float
+            The conversion factor between arc-seconds and kiloparsecs, required to plotters the unit_label in kpc.
+        unit_label_y : str
+            The y-label of the figure, which is the physical quantity being plotted.
+        xlabelsize : int
+            The fontsize of the x axes label.
+        ylabelsize : int
+            The fontsize of the y axes label.
+        xyticksize : int
+            The font size of the x and y ticks on the figure axes.
+        """
+
+        plt.ylabel(ylabel=self.label_yunits, fontsize=self.ylabelsize)
+        plt.xlabel("x (" + self.label_xunits + ")", fontsize=self.xlabelsize)
+        plt.tick_params(labelsize=self.xyticksize)
 
 
-def set_legend(plot_legend, legend_fontsize):
-    if plot_legend:
-        plt.legend(fontsize=legend_fontsize)
+    def set_xticks(self, extent):
+        """Get the extent of the dimensions of the array in the unit_label of the figure (e.g. arc-seconds or kpc).
+
+        This is used to set the extent of the array and thus the y / x axis limits.
+
+        Parameters
+        -----------
+        array : data_type.array.aa.Scaled
+            The 2D array of data_type which is plotted.
+        unit_label : str
+            The label for the unit_label of the y / x axis of the plots.
+        unit_conversion_factor : float
+            The conversion factor between arc-seconds and kiloparsecs, required to plotters the unit_label in kpc.
+        xticks_manual :  [] or None
+            If input, the xticks do not use the array's default xticks but instead overwrite them as these values.
+        yticks_manual :  [] or None
+            If input, the yticks do not use the array's default yticks but instead overwrite them as these values.
+        """
+
+        xticks = np.round(np.linspace(extent[0], extent[1], 5), 2)
+
+        if self.label_xticks is not None:
+            xtick_labels = np.asarray([self.label_xticks[0], self.label_xticks[3]])
+        elif self.unit_conversion_factor is None:
+            xtick_labels = np.round(np.linspace(extent[0], extent[1], 5), 2)
+        elif self.unit_conversion_factor is not None:
+            xtick_labels = (
+                np.round(np.linspace(extent[0], extent[1], 5), 2) * self.unit_conversion_factor
+            )
+        else:
+            raise exc.PlottingException(
+                "The y and y ticks cannot be set using the input options."
+            )
+
+        plt.xticks(ticks=xticks, labels=xtick_labels)
+
+
+    def plot_vertical_lines(self, vertical_lines, vertical_line_labels):
+
+        if vertical_lines is [] or vertical_lines is None:
+            return
+
+        for vertical_line, vertical_line_label in zip(vertical_lines, vertical_line_labels):
+
+            if self.unit_conversion_factor is None:
+                x_value_plot = vertical_line
+            else:
+                x_value_plot = vertical_line * self.unit_conversion_factor
+
+            plt.axvline(x=x_value_plot, label=vertical_line_label, linestyle="--")
+
+
+    def set_legend(self):
+        if self.include_legend:
+            plt.legend(fontsize=self.legend_fontsize)
