c6289719e17875607016d06574a906ecd63afeae
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-01-08 20:42:17 +0000

    Plane plots now work.

--- test_autoarray/unit/plots/test_mapper_plots.py
+++ test_autoarray/unit/plots/test_mapper_plots.py
@@ -24,21 +24,21 @@ def set_config_path():
 
 @pytest.fixture(name="image")
 def make_image():
-    image = aa.array.ones(shape_2d=(3, 3), pixel_scales=1.0)
-    noise_map = aa.array.full(fill_value=2.0, shape_2d=(3, 3), pixel_scales=1.0)
-    psf = aa.kernel.manual_2d(array=3.0 * np.ones((3, 3)), pixel_scales=1.0)
+    image = aa.array.ones(shape_2d=(20, 20), pixel_scales=1.0)
+    noise_map = aa.array.full(fill_value=2.0, shape_2d=(20, 20), pixel_scales=1.0)
+    psf = aa.kernel.manual_2d(array=3.0 * np.ones((20, 20)), pixel_scales=1.0)
 
     return aa.imaging.manual(image=image, noise_map=noise_map, psf=psf)
 
 
 @pytest.fixture(name="mask")
 def make_mask():
-    return aa.mask.circular(shape_2d=((3, 3)), pixel_scales=0.1, radius=0.1)
+    return aa.mask.circular(shape_2d=((20, 20)), pixel_scales=0.1, radius=0.1)
 
 
 @pytest.fixture(name="grid")
 def make_grid():
-    return aa.grid.uniform(shape_2d=(100, 100), pixel_scales=0.05, sub_size=2)
+    return aa.grid.uniform(shape_2d=(20, 20), pixel_scales=0.1, sub_size=2)
 
 
 @pytest.fixture(name="rectangular_pixelization")
@@ -61,6 +61,7 @@ def test__image_and_rectangular_mapper_is_output(
         mapper=rectangular_mapper,
         include_centres=True,
         include_grid=True,
+        include_border=False,
         image_pixels=[[0, 1, 2], [3]],
         source_pixels=[[1, 2], [0]],
         mapper_plotter=aa.plotter.mapper(
