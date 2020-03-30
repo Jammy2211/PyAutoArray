1499b7a6a674422c3b11fea57b63733906e48dff
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-03-30 15:37:38 +0100

    Fixing incorrect release

--- autoarray/__init__.py
+++ autoarray/__init__.py
@@ -33,4 +33,4 @@ from autoarray.structures.visibilities import Visibilities
 from autoarray.structures.arrays import MaskedArray
 from autoarray.structures.grids import MaskedGrid
 
-__version__ = '0.9.0'
+__version__ = "0.9.0"
