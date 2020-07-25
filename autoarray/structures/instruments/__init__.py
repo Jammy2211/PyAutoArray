f8811e5b573cde951f19b257edaaabb32b7cca86
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-07-25 15:22:56 +0100

    Removed MaskedFrame instruemnts

--- autoarray/structures/instruments/__init__.py
+++ autoarray/structures/instruments/__init__.py
@@ -3,6 +3,4 @@ from pkgutil import extend_path
 __path__ = extend_path(__path__, __name__)
 
 from .acs import FrameACS
-from .acs import MaskedFrameACS
 from .euclid import FrameEuclid
-from .euclid import MaskedFrameEuclid
