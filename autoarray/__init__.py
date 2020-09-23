93ef5b4fc6f6216482e31275fbc7e562b6ae95e2
Richard <richardhayes777@gmail.com>
2020-09-23 15:16:29 +0100

    fixing some other stuff

--- autoarray/__init__.py
+++ autoarray/__init__.py
@@ -1,3 +1,5 @@
+from autoarray.structures.grids.coordinates import GridCoordinates, GridCoordinatesUniform
+from autoarray.structures.grids.iterate import GridIterate
 from . import exc
 from . import plot
 from . import util
@@ -12,32 +14,29 @@ from .dataset.interferometer import SettingsMaskedInterferometer
 from .dataset.interferometer import SimulatorInterferometer
 from .fit.fit import FitImaging
 from .fit.fit import FitInterferometer
-from .mask.mask import Mask
-from .operators.convolver import Convolver
+from .instruments import acs
+from .instruments import euclid
 from .inversion import pixelizations as pix
 from .inversion import regularization as reg
-from .inversion.pixelizations import SettingsPixelization
-from .inversion.inversions import inversion as Inversion
 from .inversion.inversions import SettingsInversion
+from .inversion.inversions import inversion as Inversion
 from .inversion.mappers import mapper as Mapper
+from .inversion.pixelizations import SettingsPixelization
+from .mask.mask import Mask
+from .operators.convolver import Convolver
 from .operators.transformer import TransformerDFT
 from .operators.transformer import TransformerNUFFT
 from .structures.arrays import Array
 from .structures.arrays import Values
 from .structures.arrays.abstract_array import ExposureInfo
 from .structures.frames import Frame
-from .instruments import acs
-from .instruments import euclid
 from .structures.frames.abstract_frame import Scans
 from .structures.grids import Grid
-from .structures.grids import GridIterate
 from .structures.grids import GridInterpolate
 from .structures.grids import GridRectangular
 from .structures.grids import GridVoronoi
-from .structures.grids import GridCoordinates
-from .structures.grids import GridCoordinatesUniform
-from .structures.region import Region
 from .structures.kernel import Kernel
+from .structures.region import Region
 from .structures.visibilities import Visibilities
 from .structures.visibilities import VisibilitiesNoiseMap
 
