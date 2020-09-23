d6312cf14f58a734587cc7c7c88e3b567d5ebbd8
Richard <richardhayes777@gmail.com>
2020-09-23 15:16:29 +0100

    fixing some other stuff

--- autoarray/structures/grids/__init__.py
+++ autoarray/structures/grids/__init__.py
@@ -1,6 +1,6 @@
 from .grids import Grid, GridSparse, GridTransformed, GridTransformedNumpy
-from .iterate import GridIterate
+# from .iterate import GridIterate
 from .interpolate import GridInterpolate
-from .coordinates import GridCoordinates, GridCoordinatesUniform
+# from .coordinates import GridCoordinates, GridCoordinatesUniform
 from .pixelization import GridRectangular, GridVoronoi
 from .decorators import *
