8f715d2355e8439b7f9e10ac23350e28444d3b45
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-01-21 20:21:00 +0000

    docs

---
+++ test_autoarray/unit/structures/lines/test_lines.py
@@ -0,0 +1,29 @@
+from os import path
+import numpy as np
+import pytest
+
+import autoarray as aa
+from autoarray import exc
+from autoarray.structures import lines
+
+test_line_dir = path.join(
+    "{}".format(path.dirname(path.realpath(__file__))), "files", "lines"
+)
+
+
+class TestAPI:
+    def test__manual__makes_line_with_pixel_scale(self):
+
+        line = aa.Line.manual_1d(line=[1.0, 2.0, 3.0, 4.0], pixel_scales=1.0)
+
+        assert type(line) == lines.Line
+        assert (line.in_1d == np.array([1.0, 2.0, 3.0, 4.0])).all()
+        assert line.pixel_scales == (1.0, 1.0)
+        assert line.origin == (0.0, 0.0)
+
+        line = aa.Line.manual_1d(
+            line=[[1.0, 2.0], [3.0, 4.0], [5.0, 6.0], [7.0, 8.0]],
+            shape_2d=(2, 2),
+            pixel_scales=1.0,
+            origin=(0.0, 1.0),
+        )
