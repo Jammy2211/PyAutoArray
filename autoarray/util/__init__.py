6a9be6fd4ddc30c9c5c3993eadb486b05adb202f
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-02-13 17:06:21 +0000

    moved line util

--- autoarray/util/__init__.py
+++ autoarray/util/__init__.py
@@ -5,11 +5,11 @@ from ..structures.arrays import array_util as array
 from ..structures.grids.one_d import grid_1d_util as grid_1d
 from ..structures.grids.two_d import grid_2d_util as grid_2d
 from ..structures.frames import frame_util as frame
+from ..structures.lines import line_util as line
 from ..fit import fit_util as fit
 from ..inversion import pixelization_util as pixelization
 from ..inversion import mapper_util as mapper
 from ..inversion import regularization_util as regularization
 from ..inversion import inversion_util as inversion
-from . import line_util as line
 from . import sparse_util as sparse
 from . import transformer_util as transformer
