c21e8859bdb20737352147b9904797ac99985b73
Jonathan Frawley <jonathan.frawley@durham.ac.uk>
2021-05-12 15:23:47 +0100

    Remove commented out line

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ with open(join(this_dir, "requirements.txt")) as f:
     requirements = f.read().split("\n")
 
 version = environ.get("VERSION", "1.0.dev0")
-#requirements.extend([f"autoconf=={version}"])
 requirements.extend([f"autoconf=={version}"])
 
 setup(
