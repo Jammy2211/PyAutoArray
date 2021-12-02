1094199bda730821380408a68f860d32709250a2
James Nightingale <james.w.nightingale@durham.ac.uk>
2021-12-02 10:48:28 +0000

    moved WTildeInterferometer to interferometer.py dataset module

--- autoarray/dataset/interferometer.py
+++ autoarray/dataset/interferometer.py
@@ -3,6 +3,9 @@ import numpy as np
 import copy
 from typing import List, Optional
 
+from autoconf import cached_property
+
+from autoarray.dataset.abstract_dataset import AbstractWTilde
 from autoarray.dataset.abstract_dataset import AbstractSettingsDataset
 from autoarray.dataset.abstract_dataset import AbstractDataset
 from autoarray.structures.grids.two_d.grid_2d import Grid2D
@@ -14,10 +17,17 @@ from autoarray.structures.visibilities import VisibilitiesNoiseMap
 from autoarray import exc
 from autoarray.structures.arrays.two_d import array_2d_util
 from autoarray.dataset import preprocess
-
+from autoarray.inversion.inversion import inversion_interferometer_util
 
 logger = logging.getLogger(__name__)
 
+
+class WTildeInterferometer(AbstractWTilde):
+    def __init__(self, curvature_preload, noise_map_value):
+
+        super().__init__(
+            curvature_preload=curvature_preload, noise_map_value=noise_map_value
+        )
 
 class SettingsInterferometer(AbstractSettingsDataset):
     def __init__(
@@ -147,6 +157,37 @@ class Interferometer(AbstractDataset):
             real_space_mask=self.real_space_mask,
             settings=settings,
             name=self.name,
+        )
+
+    @cached_property
+    def w_tilde(self):
+        """
+        The w_tilde formalism of the linear algebra equations precomputes the Fourier Transform of all the visibilities
+        given the `uv_wavelengths` (see `inversion.inversion_util`).
+
+        The `WTilde` object stores these precomputed values in the interferometer dataset ensuring they are only
+        computed once per analysis.
+
+        This uses lazy allocation such that the calculation is only performed when the wtilde matrices are used,
+        ensuring efficient set up of the `Interferometer` class.
+
+        Returns
+        -------
+        WTildeInterferometer
+            Precomputed values used for the w tilde formalism of linear algebra calculations.
+        """
+
+        logger.info("IMAGING - Computing W-Tilde... May take a moment.")
+
+        curvature_preload = inversion_interferometer_util.w_tilde_curvature_preload_interferometer_from(
+            noise_map_real=self.noise_map.real,
+            uv_wavelengths=self.uv_wavelengths,
+            shape_masked_pixels_2d=self.transformer.grid.mask.shape_native_masked_pixels,
+            grid_radians_2d=self.transformer.grid.mask.unmasked_grid_sub_1.in_radians.native,
+        )
+
+        return WTildeInterferometer(
+            curvature_preload=curvature_preload, noise_map_value=self.noise_map[0]
         )
 
     @property
