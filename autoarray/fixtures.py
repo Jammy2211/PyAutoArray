e6648baf5fcaf438efbf321a499c063b7780cd4f
James Nightingale <james.w.nightingale@durham.ac.uk>
2022-03-05 15:35:58 +0000

    refactored fixztures

--- autoarray/fixtures.py
+++ autoarray/fixtures.py
@@ -1,37 +1,6 @@
 import numpy as np
 
-from autoarray.structures.arrays.one_d.array_1d import Array1D
-from autoarray.structures.arrays.two_d.array_2d import Array2D
-from autoarray.dataset.interferometer import SettingsInterferometer
-from autoarray.inversion.regularization.constant import Constant
-from autoarray.inversion.regularization.constant import ConstantSplit
-from autoarray.inversion.regularization.adaptive_brightness import AdaptiveBrightness
-from autoarray.inversion.regularization.adaptive_brightness import (
-    AdaptiveBrightnessSplit,
-)
-from autoarray.operators.convolver import Convolver
-from autoarray.structures.grids.one_d.grid_1d import Grid1D
-from autoarray.structures.grids.two_d.grid_2d import Grid2D
-from autoarray.structures.grids.two_d.grid_2d_iterate import Grid2DIterate
-from autoarray.structures.grids.two_d.grid_2d_irregular import Grid2DIrregular
-from autoarray.structures.grids.two_d.grid_2d_pixelization import Grid2DRectangular
-from autoarray.structures.grids.two_d.grid_2d_pixelization import Grid2DDelaunay
-from autoarray.structures.grids.two_d.grid_2d_pixelization import Grid2DVoronoi
-from autoarray.dataset.imaging import Imaging
-from autoarray.dataset.interferometer import Interferometer
-from autoarray.structures.kernel_2d import Kernel2D
-from autoarray.layout.layout import Layout2D
-from autoarray.inversion.mappers.rectangular import MapperRectangularNoInterp
-from autoarray.inversion.mappers.delaunay import MapperDelaunay
-from autoarray.inversion.mappers.voronoi import MapperVoronoiNoInterp
-from autoarray.inversion.mappers.voronoi import MapperVoronoi
-from autoarray.mask.mask_1d import Mask1D
-from autoarray.mask.mask_2d import Mask2D
-from autoarray.operators.transformer import TransformerDFT
-from autoarray.operators.transformer import TransformerNUFFT
-from autoarray.structures.visibilities import Visibilities
-from autoarray.structures.visibilities import VisibilitiesNoiseMap
-from autoarray.inversion.inversion.factory import inversion_from
+import autoarray as aa
 
 from autoarray.fit.mock.mock_fit_imaging import MockFitImaging
 from autoarray.fit.mock.mock_fit_interferometer import MockFitInterferometer
@@ -41,14 +10,14 @@ def make_mask_1d_7():
 
     mask = np.array([True, True, False, False, False, True, True])
 
-    return Mask1D.manual(mask=mask, pixel_scales=(1.0,), sub_size=1)
+    return aa.Mask1D.manual(mask=mask, pixel_scales=(1.0,), sub_size=1)
 
 
 def make_sub_mask_1d_7():
 
     mask = np.array([True, True, False, False, False, True, True])
 
-    return Mask1D.manual(mask=mask, pixel_scales=(1.0,), sub_size=2)
+    return aa.Mask1D.manual(mask=mask, pixel_scales=(1.0,), sub_size=2)
 
 
 def make_mask_2d_7x7():
@@ -65,7 +34,7 @@ def make_mask_2d_7x7():
         ]
     )
 
-    return Mask2D.manual(mask=mask, pixel_scales=(1.0, 1.0), sub_size=1)
+    return aa.Mask2D.manual(mask=mask, pixel_scales=(1.0, 1.0), sub_size=1)
 
 
 def make_sub_mask_2d_7x7():
@@ -81,7 +50,7 @@ def make_sub_mask_2d_7x7():
         ]
     )
 
-    return Mask2D.manual(mask=mask, sub_size=2, pixel_scales=(1.0, 1.0))
+    return aa.Mask2D.manual(mask=mask, sub_size=2, pixel_scales=(1.0, 1.0))
 
 
 def make_mask_2d_7x7_1_pix():
@@ -97,7 +66,7 @@ def make_mask_2d_7x7_1_pix():
         ]
     )
 
-    return Mask2D.manual(mask=mask, pixel_scales=(1.0, 1.0))
+    return aa.Mask2D.manual(mask=mask, pixel_scales=(1.0, 1.0))
 
 
 def make_blurring_mask_2d_7x7():
@@ -113,22 +82,22 @@ def make_blurring_mask_2d_7x7():
         ]
     )
 
-    return Mask2D.manual(mask=blurring_mask, pixel_scales=(1.0, 1.0))
+    return aa.Mask2D.manual(mask=blurring_mask, pixel_scales=(1.0, 1.0))
 
 
 ### arrays ###
 
 
 def make_array_1d_7():
-    return Array1D.ones(shape_native=(7,), pixel_scales=(1.0,))
+    return aa.Array1D.ones(shape_native=(7,), pixel_scales=(1.0,))
 
 
 def make_array_2d_7x7():
-    return Array2D.ones(shape_native=(7, 7), pixel_scales=(1.0, 1.0))
+    return aa.Array2D.ones(shape_native=(7, 7), pixel_scales=(1.0, 1.0))
 
 
 def make_layout_2d_7x7():
-    return Layout2D(
+    return aa.Layout2D(
         shape_2d=(7, 7),
         original_roe_corner=(1, 0),
         serial_overscan=(0, 6, 6, 7),
@@ -141,23 +110,23 @@ def make_layout_2d_7x7():
 
 
 def make_grid_1d_7():
-    return Grid1D.from_mask(mask=make_mask_1d_7())
+    return aa.Grid1D.from_mask(mask=make_mask_1d_7())
 
 
 def make_sub_grid_1d_7():
-    return Grid1D.from_mask(mask=make_sub_mask_1d_7())
+    return aa.Grid1D.from_mask(mask=make_sub_mask_1d_7())
 
 
 def make_grid_2d_7x7():
-    return Grid2D.from_mask(mask=make_mask_2d_7x7())
+    return aa.Grid2D.from_mask(mask=make_mask_2d_7x7())
 
 
 def make_sub_grid_2d_7x7():
-    return Grid2D.from_mask(mask=make_sub_mask_2d_7x7())
+    return aa.Grid2D.from_mask(mask=make_sub_mask_2d_7x7())
 
 
 def make_grid_2d_iterate_7x7():
-    return Grid2DIterate.from_mask(
+    return aa.Grid2DIterate.from_mask(
         mask=make_mask_2d_7x7(), fractional_accuracy=0.9999, sub_steps=[2, 4, 8, 16]
     )
 
@@ -172,48 +141,48 @@ def make_sub_grid_2d_7x7_simple():
 
 
 def make_blurring_grid_2d_7x7():
-    return Grid2D.from_mask(mask=make_blurring_mask_2d_7x7())
+    return aa.Grid2D.from_mask(mask=make_blurring_mask_2d_7x7())
 
 
 # CONVOLVERS #
 
 
 def make_convolver_7x7():
-    return Convolver(mask=make_mask_2d_7x7(), kernel=make_psf_3x3())
+    return aa.Convolver(mask=make_mask_2d_7x7(), kernel=make_psf_3x3())
 
 
 def make_image_7x7():
-    return Array2D.ones(shape_native=(7, 7), pixel_scales=(1.0, 1.0))
+    return aa.Array2D.ones(shape_native=(7, 7), pixel_scales=(1.0, 1.0))
 
 
 def make_psf_3x3():
 
     psf = np.array([[0.0, 0.5, 0.0], [0.5, 1.0, 0.5], [0.0, 0.5, 0.0]])
 
-    return Kernel2D.manual_native(array=psf, pixel_scales=(1.0, 1.0))
+    return aa.Kernel2D.manual_native(array=psf, pixel_scales=(1.0, 1.0))
 
 
 def make_psf_3x3_no_blur():
-    return Kernel2D.no_blur(pixel_scales=(1.0, 1.0))
+    return aa.Kernel2D.no_blur(pixel_scales=(1.0, 1.0))
 
 
 def make_noise_map_7x7():
-    return Array2D.full(fill_value=2.0, shape_native=(7, 7), pixel_scales=(1.0, 1.0))
+    return aa.Array2D.full(fill_value=2.0, shape_native=(7, 7), pixel_scales=(1.0, 1.0))
 
 
 def make_grid_2d_irregular_7x7():
-    return Grid2DIrregular(grid=[(0.1, 0.1), (0.2, 0.2)])
+    return aa.Grid2DIrregular(grid=[(0.1, 0.1), (0.2, 0.2)])
 
 
 def make_grid_2d_irregular_7x7_list():
     return [
-        Grid2DIrregular(grid=[(0.1, 0.1), (0.2, 0.2)]),
-        Grid2DIrregular(grid=[(0.3, 0.3)]),
+        aa.Grid2DIrregular(grid=[(0.1, 0.1), (0.2, 0.2)]),
+        aa.Grid2DIrregular(grid=[(0.3, 0.3)]),
     ]
 
 
 def make_imaging_7x7():
-    return Imaging(
+    return aa.Imaging(
         image=make_image_7x7(),
         psf=make_psf_3x3(),
         noise_map=make_noise_map_7x7(),
@@ -222,7 +191,7 @@ def make_imaging_7x7():
 
 
 def make_imaging_7x7_no_blur():
-    return Imaging(
+    return aa.Imaging(
         image=make_image_7x7(),
         psf=make_psf_3x3_no_blur(),
         noise_map=make_noise_map_7x7(),
@@ -231,11 +200,11 @@ def make_imaging_7x7_no_blur():
 
 
 def make_visibilities_7():
-    return Visibilities.full(shape_slim=(7,), fill_value=1.0)
+    return aa.Visibilities.full(shape_slim=(7,), fill_value=1.0)
 
 
 def make_visibilities_noise_map_7():
-    return VisibilitiesNoiseMap.full(shape_slim=(7,), fill_value=2.0)
+    return aa.VisibilitiesNoiseMap.full(shape_slim=(7,), fill_value=2.0)
 
 
 def make_uv_wavelengths_7x2():
@@ -257,54 +226,56 @@ def make_uv_wavelengths_7x2_no_fft():
 
 
 def make_interferometer_7():
-    return Interferometer(
+    return aa.Interferometer(
         visibilities=make_visibilities_7(),
         noise_map=make_visibilities_noise_map_7(),
         uv_wavelengths=make_uv_wavelengths_7x2(),
         real_space_mask=make_sub_mask_2d_7x7(),
-        settings=SettingsInterferometer(
-            grid_class=Grid2D, sub_size=1, transformer_class=TransformerDFT
+        settings=aa.SettingsInterferometer(
+            grid_class=aa.Grid2D, sub_size=1, transformer_class=aa.TransformerDFT
         ),
     )
 
 
 def make_interferometer_7_no_fft():
-    return Interferometer(
+    return aa.Interferometer(
         visibilities=make_visibilities_7(),
         noise_map=make_visibilities_noise_map_7(),
         uv_wavelengths=make_uv_wavelengths_7x2_no_fft(),
         real_space_mask=make_sub_mask_2d_7x7(),
-        settings=SettingsInterferometer(
-            grid_class=Grid2D, sub_size=1, transformer_class=TransformerDFT
+        settings=aa.SettingsInterferometer(
+            grid_class=aa.Grid2D, sub_size=1, transformer_class=aa.TransformerDFT
         ),
     )
 
 
 def make_interferometer_7_grid():
-    return Interferometer(
+    return aa.Interferometer(
         visibilities=make_visibilities_7(),
         noise_map=make_visibilities_noise_map_7(),
         uv_wavelengths=make_uv_wavelengths_7x2(),
         real_space_mask=make_sub_mask_2d_7x7(),
-        settings=SettingsInterferometer(sub_size=1, transformer_class=TransformerDFT),
+        settings=aa.SettingsInterferometer(
+            sub_size=1, transformer_class=aa.TransformerDFT
+        ),
     )
 
 
 def make_interferometer_7_lop():
 
-    return Interferometer(
+    return aa.Interferometer(
         visibilities=make_visibilities_7(),
         noise_map=make_visibilities_noise_map_7(),
         uv_wavelengths=make_uv_wavelengths_7x2(),
         real_space_mask=make_mask_2d_7x7(),
-        settings=SettingsInterferometer(
-            sub_size_inversion=1, transformer_class=TransformerNUFFT
+        settings=aa.SettingsInterferometer(
+            sub_size_inversion=1, transformer_class=aa.TransformerNUFFT
         ),
     )
 
 
 def make_transformer_7x7_7():
-    return TransformerDFT(
+    return aa.TransformerDFT(
         uv_wavelengths=make_uv_wavelengths_7x2(), real_space_mask=make_mask_2d_7x7()
     )
 
@@ -349,34 +320,34 @@ def make_fit_interferometer_7():
 
 
 def make_regularization_constant():
-    return Constant(coefficient=1.0)
+    return aa.reg.Constant(coefficient=1.0)
 
 
 def make_regularization_constant_split():
-    return ConstantSplit(coefficient=1.0)
+    return aa.reg.ConstantSplit(coefficient=1.0)
 
 
 def make_regularization_adaptive_brightness():
-    return AdaptiveBrightness(
+    return aa.reg.AdaptiveBrightness(
         inner_coefficient=0.1, outer_coefficient=10.0, signal_scale=0.5
     )
 
 
 def make_regularization_adaptive_brightness_split():
-    return AdaptiveBrightnessSplit(
+    return aa.reg.AdaptiveBrightnessSplit(
         inner_coefficient=0.1, outer_coefficient=10.0, signal_scale=0.5
     )
 
 
 def make_rectangular_pixelization_grid_3x3():
-    return Grid2DRectangular.overlay_grid(
+    return aa.Grid2DRectangular.overlay_grid(
         grid=make_sub_grid_2d_7x7(), shape_native=(3, 3)
     )
 
 
 def make_delaunay_pixelization_grid_9():
 
-    grid_9 = Grid2D.manual_slim(
+    grid_9 = aa.Grid2D.manual_slim(
         grid=[
             [0.6, -0.3],
             [0.5, -0.8],
@@ -392,12 +363,12 @@ def make_delaunay_pixelization_grid_9():
         pixel_scales=1.0,
     )
 
-    return Grid2DDelaunay(grid=grid_9)
+    return aa.Grid2DDelaunay(grid=grid_9)
 
 
 def make_voronoi_pixelization_grid_9():
 
-    grid_9 = Grid2D.manual_slim(
+    grid_9 = aa.Grid2D.manual_slim(
         grid=[
             [0.6, -0.3],
             [0.5, -0.8],
@@ -413,7 +384,7 @@ def make_voronoi_pixelization_grid_9():
         pixel_scales=1.0,
     )
 
-    return Grid2DVoronoi(
+    return aa.Grid2DVoronoi(
         grid=grid_9,
         nearest_pixelization_index_for_slim_index=np.zeros(
             shape=make_grid_2d_7x7().shape_slim, dtype="int"
@@ -422,44 +393,44 @@ def make_voronoi_pixelization_grid_9():
 
 
 def make_rectangular_mapper_7x7_3x3():
-    return MapperRectangularNoInterp(
+    return aa.MapperRectangularNoInterp(
         source_grid_slim=make_sub_grid_2d_7x7(),
         source_pixelization_grid=make_rectangular_pixelization_grid_3x3(),
         data_pixelization_grid=None,
-        hyper_image=Array2D.ones(shape_native=(3, 3), pixel_scales=0.1),
+        hyper_image=aa.Array2D.ones(shape_native=(3, 3), pixel_scales=0.1),
     )
 
 
 def make_delaunay_mapper_9_3x3():
-    return MapperDelaunay(
+    return aa.MapperDelaunay(
         source_grid_slim=make_sub_grid_2d_7x7(),
         source_pixelization_grid=make_delaunay_pixelization_grid_9(),
-        data_pixelization_grid=Grid2D.uniform(shape_native=(3, 3), pixel_scales=0.1),
-        hyper_image=Array2D.ones(shape_native=(3, 3), pixel_scales=0.1),
+        data_pixelization_grid=aa.Grid2D.uniform(shape_native=(3, 3), pixel_scales=0.1),
+        hyper_image=aa.Array2D.ones(shape_native=(3, 3), pixel_scales=0.1),
     )
 
 
 def make_voronoi_mapper_9_3x3():
-    return MapperVoronoiNoInterp(
+    return aa.MapperVoronoiNoInterp(
         source_grid_slim=make_sub_grid_2d_7x7(),
         source_pixelization_grid=make_voronoi_pixelization_grid_9(),
-        data_pixelization_grid=Grid2D.uniform(shape_native=(3, 3), pixel_scales=0.1),
-        hyper_image=Array2D.ones(shape_native=(3, 3), pixel_scales=0.1),
+        data_pixelization_grid=aa.Grid2D.uniform(shape_native=(3, 3), pixel_scales=0.1),
+        hyper_image=aa.Array2D.ones(shape_native=(3, 3), pixel_scales=0.1),
     )
 
 
 def make_voronoi_mapper_nn_9_3x3():
-    return MapperVoronoi(
+    return aa.MapperVoronoi(
         source_grid_slim=make_sub_grid_2d_7x7(),
         source_pixelization_grid=make_voronoi_pixelization_grid_9(),
-        data_pixelization_grid=Grid2D.uniform(shape_native=(3, 3), pixel_scales=0.1),
-        hyper_image=Array2D.ones(shape_native=(3, 3), pixel_scales=0.1),
+        data_pixelization_grid=aa.Grid2D.uniform(shape_native=(3, 3), pixel_scales=0.1),
+        hyper_image=aa.Array2D.ones(shape_native=(3, 3), pixel_scales=0.1),
     )
 
 
 def make_rectangular_inversion_7x7_3x3():
 
-    return inversion_from(
+    return aa.Inversion(
         dataset=make_masked_imaging_7x7(),
         linear_obj_list=[make_rectangular_mapper_7x7_3x3()],
         regularization_list=[make_regularization_constant()],
@@ -468,7 +439,7 @@ def make_rectangular_inversion_7x7_3x3():
 
 def make_delaunay_inversion_9_3x3():
 
-    return inversion_from(
+    return aa.Inversion(
         dataset=make_masked_imaging_7x7(),
         linear_obj_list=[make_delaunay_mapper_9_3x3()],
         regularization_list=[make_regularization_constant()],
@@ -477,7 +448,7 @@ def make_delaunay_inversion_9_3x3():
 
 def make_voronoi_inversion_9_3x3():
 
-    return inversion_from(
+    return aa.Inversion(
         dataset=make_masked_imaging_7x7(),
         linear_obj_list=[make_voronoi_mapper_9_3x3()],
         regularization_list=[make_regularization_constant()],
