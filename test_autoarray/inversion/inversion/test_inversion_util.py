4daf0278b994dfa7d4a4256dd48ac35f53eed6f4
James Nightingale <james.w.nightingale@durham.ac.uk>
2022-07-21 15:46:45 +0100

    moved final leq tests

--- test_autoarray/inversion/inversion/test_inversion_util.py
+++ test_autoarray/inversion/inversion/test_inversion_util.py
@@ -87,7 +87,9 @@ def test__curvature_matrix_via_sparse_preload():
         curvature_matrix_counts=curvature_matrix_counts.astype("int"),
     )
 
-    assert curvature_matrix_via_mapping_matrix == pytest.approx(curvature_matrix, 1.0e-4)
+    assert curvature_matrix_via_mapping_matrix == pytest.approx(
+        curvature_matrix, 1.0e-4
+    )
 
 
 def test__curvature_matrix_via_mapping_matrix_from():
@@ -135,6 +137,80 @@ def test__curvature_matrix_via_mapping_matrix_from():
         curvature_matrix
         == np.array([[1.25, 0.25, 0.0], [0.25, 2.25, 1.0], [0.0, 1.0, 1.0]])
     ).all()
+
+
+def test__mapped_reconstructed_data_via_mapping_matrix_from():
+
+    mapping_matrix = np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+
+    reconstruction = np.array([1.0, 1.0, 2.0])
+
+    mapped_reconstructed_data = aa.util.inversion.mapped_reconstructed_data_via_mapping_matrix_from(
+        mapping_matrix=mapping_matrix, reconstruction=reconstruction
+    )
+
+    assert (mapped_reconstructed_data == np.array([1.0, 1.0, 2.0])).all()
+
+    mapping_matrix = np.array([[0.25, 0.50, 0.25], [0.0, 1.0, 0.0], [0.0, 0.25, 0.75]])
+
+    reconstruction = np.array([1.0, 1.0, 2.0])
+
+    mapped_reconstructed_data = aa.util.inversion.mapped_reconstructed_data_via_mapping_matrix_from(
+        mapping_matrix=mapping_matrix, reconstruction=reconstruction
+    )
+
+    assert (mapped_reconstructed_data == np.array([1.25, 1.0, 1.75])).all()
+
+
+def test__mapped_reconstructed_data_via_image_to_pix_unique_from():
+
+    pix_indexes_for_sub_slim_index = np.array([[0], [1], [2]])
+    pix_indexes_for_sub_slim_index_sizes = np.array([1, 1, 1]).astype("int")
+    pix_weights_for_sub_slim_index = np.array([[1.0], [1.0], [1.0]])
+
+    data_to_pix_unique, data_weights, pix_lengths = aa.util.mapper.data_slim_to_pixelization_unique_from(
+        data_pixels=3,
+        pix_indexes_for_sub_slim_index=pix_indexes_for_sub_slim_index,
+        pix_sizes_for_sub_slim_index=pix_indexes_for_sub_slim_index_sizes,
+        pix_weights_for_sub_slim_index=pix_weights_for_sub_slim_index,
+        sub_size=1,
+    )
+
+    reconstruction = np.array([1.0, 1.0, 2.0])
+
+    mapped_reconstructed_data = aa.util.inversion.mapped_reconstructed_data_via_image_to_pix_unique_from(
+        data_to_pix_unique=data_to_pix_unique.astype("int"),
+        data_weights=data_weights,
+        pix_lengths=pix_lengths.astype("int"),
+        reconstruction=reconstruction,
+    )
+
+    assert (mapped_reconstructed_data == np.array([1.0, 1.0, 2.0])).all()
+
+    pix_indexes_for_sub_slim_index = np.array(
+        [[0], [1], [1], [2], [1], [1], [1], [1], [1], [2], [2], [2]]
+    )
+    pix_indexes_for_sub_slim_index_sizes = np.ones(shape=(12,)).astype("int")
+    pix_weights_for_sub_slim_index = np.ones(shape=(12, 1))
+
+    data_to_pix_unique, data_weights, pix_lengths = aa.util.mapper.data_slim_to_pixelization_unique_from(
+        data_pixels=3,
+        pix_indexes_for_sub_slim_index=pix_indexes_for_sub_slim_index,
+        pix_sizes_for_sub_slim_index=pix_indexes_for_sub_slim_index_sizes,
+        pix_weights_for_sub_slim_index=pix_weights_for_sub_slim_index,
+        sub_size=2,
+    )
+
+    reconstruction = np.array([1.0, 1.0, 2.0])
+
+    mapped_reconstructed_data = aa.util.inversion.mapped_reconstructed_data_via_image_to_pix_unique_from(
+        data_to_pix_unique=data_to_pix_unique.astype("int"),
+        data_weights=data_weights,
+        pix_lengths=pix_lengths.astype("int"),
+        reconstruction=reconstruction,
+    )
+
+    assert (mapped_reconstructed_data == np.array([1.25, 1.0, 1.75])).all()
 
 
 def test__preconditioner_matrix_via_mapping_matrix_from():
