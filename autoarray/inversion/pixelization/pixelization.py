206f2774e9aa97926c31bd4d4d5d1e29f05e90d8
James Nightingale <james.w.nightingale@durham.ac.uk>
2022-07-24 16:48:50 +0100

    optional type on Pixelization

--- autoarray/inversion/pixelization/pixelization.py
+++ autoarray/inversion/pixelization/pixelization.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from autoarray.inversion.pixelization.mesh.abstract import AbstractMesh
 from autoarray.inversion.regularization.abstract import AbstractRegularization
 
 
 class Pixelization:
-    def __init__(self, mesh: AbstractMesh, regularization: AbstractRegularization):
+    def __init__(self, mesh: AbstractMesh, regularization: Optional[AbstractRegularization] = None):
 
         self.mesh = mesh
         self.regularization = regularization
