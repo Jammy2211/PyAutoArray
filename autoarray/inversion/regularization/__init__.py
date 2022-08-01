1b94431844f5dae4de4e66d73a174a1eda78dd1e
James Nightingale <james.w.nightingale@durham.ac.uk>
2022-08-01 16:06:06 +0100

    import

--- autoarray/inversion/regularization/__init__.py
+++ autoarray/inversion/regularization/__init__.py
@@ -1,4 +1,5 @@
 from .abstract import AbstractRegularization as Regularization
+from .zeroth import Zeroth
 from .constant import Constant
 from .constant_split import ConstantSplit
 from .adaptive_brightness import AdaptiveBrightness
