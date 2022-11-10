ca00b918a0500ad4df8fa3c3c9aa2de4ea077d5a
Jammy2211 <james.w.nightingale@durham.ac.uk>
2022-11-10 18:28:01 +0000

    undo horror changes

--- test_autoarray/plot/mat_wrap/test_mat_plot.py
+++ test_autoarray/plot/mat_wrap/test_mat_plot.py
@@ -19,3 +19,21 @@ def test__add_mat_plot_objects_together():
 
     assert mat_plot_2d.axis.config_dict["extent"] == extent
     assert mat_plot_2d.ylabel.config_dict["fontsize"] == 20
+
+    units = aplt.Units(in_kpc=True)
+    output = aplt.Output(format="png")
+
+    mat_plot_2d_0 = aplt.MatPlot2D(
+        axis=aplt.Axis(extent=extent), units=units, output=output
+    )
+    mat_plot_2d_1 = aplt.MatPlot2D(ylabel=aplt.YLabel(fontsize=fontsize))
+
+    mat_plot_2d = mat_plot_2d_0 + mat_plot_2d_1
+
+    assert mat_plot_2d.units.in_kpc == True
+    assert mat_plot_2d.output.format == "png"
+
+    mat_plot_2d = mat_plot_2d_1 + mat_plot_2d_0
+
+    assert mat_plot_2d.units.in_kpc == True
+    assert mat_plot_2d.output.format == "png"
