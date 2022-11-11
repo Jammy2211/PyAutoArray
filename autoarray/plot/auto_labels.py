93a476edf1deef941853e0f327815ab56ec3ffa5
Jammy2211 <james.w.nightingale@durham.ac.uk>
2022-11-11 13:19:23 +0000

    split up mat_plot into package

---
+++ autoarray/plot/auto_labels.py
@@ -0,0 +1,10 @@
+class AutoLabels:
+    def __init__(
+        self, title=None, ylabel=None, xlabel=None, legend=None, filename=None
+    ):
+
+        self.title = title
+        self.ylabel = ylabel
+        self.xlabel = xlabel
+        self.legend = legend
+        self.filename = filename
