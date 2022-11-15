6f5c46f1e8a7ff396b3a4debd94a6aa195d6bbed
Jammy2211 <james.w.nightingale@durham.ac.uk>
2022-11-15 18:23:22 +0000

    coefficient neighbor consistent spellings

--- autoarray/inversion/regularization/constant_zeroth.py
+++ autoarray/inversion/regularization/constant_zeroth.py
@@ -6,19 +6,19 @@ from autoarray.inversion.regularization import regularization_util
 
 
 class ConstantZeroth(AbstractRegularization):
-    def __init__(self, coefficient_neighbour=1.0, coefficient_zeroth=1.0):
+    def __init__(self, coefficient_neighbor=1.0, coefficient_zeroth=1.0):
 
         super().__init__()
 
-        self.coefficient_neighbour = coefficient_neighbour
+        self.coefficient_neighbor = coefficient_neighbor
         self.coefficient_zeroth = coefficient_zeroth
 
     def regularization_weights_from(self, linear_obj) -> np.ndarray:
-        return self.coefficient_neighbour * np.ones(linear_obj.pixels)
+        return self.coefficient_neighbor * np.ones(linear_obj.pixels)
 
     def regularization_matrix_from(self, linear_obj):
         return regularization_util.constant_zeroth_regularization_matrix_from(
-            coefficient=self.coefficient_neighbour,
+            coefficient=self.coefficient_neighbor,
             coefficient_zeroth=self.coefficient_zeroth,
             neighbors=linear_obj.neighbors,
             neighbors_sizes=linear_obj.neighbors.sizes,
