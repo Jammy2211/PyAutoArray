5dbcd87f5be6747fa0255512eeff6771f4180a5d
Jammy2211 <james.w.nightingale@durham.ac.uk>
2022-11-15 13:48:21 +0000

    ConstantZeroth import

--- autoarray/inversion/regularization/__init__.py
+++ autoarray/inversion/regularization/__init__.py
@@ -1,6 +1,7 @@
 from .abstract import AbstractRegularization as Regularization
 from .zeroth import Zeroth
 from .constant import Constant
+from .constant_zeroth import ConstantZeroth
 from .constant_split import ConstantSplit
 from .adaptive_brightness import AdaptiveBrightness
 from .adaptive_brightness_split import AdaptiveBrightnessSplit
