68c5ba91b252f035f3c41de0a4e3f542b84d79d2
Jammy2211 <james.w.nightingale@durham.ac.uk>
2022-12-23 15:16:11 +0000

    refactor test_mask_1d.py

--- test_autoarray/mask/test_mask_1d.py
+++ test_autoarray/mask/test_mask_1d.py
@@ -5,129 +5,106 @@ import autoarray as aa
 from autoarray import exc
 
 
-class TestMask1D:
-    def test__mask__makes_mask_with_pixel_scale(self):
+def test__manual():
 
-        mask = aa.Mask1D.manual(mask=[False, True], pixel_scales=1.0)
+    mask = aa.Mask1D.manual(mask=[False, True], pixel_scales=1.0)
 
-        assert type(mask) == aa.Mask1D
-        assert (mask == np.array([False, True])).all()
-        assert mask.pixel_scale == 1.0
-        assert mask.pixel_scales == (1.0,)
-        assert mask.origin == (0.0,)
-        assert (mask.extent == np.array([-1.0, 1.0])).all()
+    assert type(mask) == aa.Mask1D
+    assert (mask == np.array([False, True])).all()
+    assert mask.pixel_scale == 1.0
+    assert mask.pixel_scales == (1.0,)
+    assert mask.origin == (0.0,)
+    assert (mask.extent == np.array([-1.0, 1.0])).all()
 
-        mask = aa.Mask1D.manual(
-            mask=[False, False, True], pixel_scales=3.0, origin=(1.0,)
-        )
+    mask = aa.Mask1D.manual(mask=[False, False, True], pixel_scales=3.0, origin=(1.0,))
 
-        assert type(mask) == aa.Mask1D
-        assert (mask == np.array([False, False, True])).all()
-        assert mask.pixel_scale == 3.0
-        assert mask.origin == (1.0,)
-        assert (mask.extent == np.array([-3.5, 5.5])).all()
+    assert type(mask) == aa.Mask1D
+    assert (mask == np.array([False, False, True])).all()
+    assert mask.pixel_scale == 3.0
+    assert mask.origin == (1.0,)
+    assert (mask.extent == np.array([-3.5, 5.5])).all()
 
-    def test__mask__makes_mask_with_pixel_scale_and_sub_size(self):
+    mask = aa.Mask1D.manual(
+        mask=[False, False, True, True], pixel_scales=3.0, sub_size=2, origin=(1.0,)
+    )
 
-        mask = aa.Mask1D.manual(
-            mask=[False, False, True, True], pixel_scales=1.0, sub_size=1
-        )
+    assert type(mask) == aa.Mask1D
+    assert (mask == np.array([False, False, True, True])).all()
+    assert mask.pixel_scale == 3.0
+    assert mask.origin == (1.0,)
+    assert mask.sub_size == 2
 
-        assert type(mask) == aa.Mask1D
-        assert (mask == np.array([False, False, True, True])).all()
-        assert mask.pixel_scale == 1.0
-        assert mask.origin == (0.0,)
-        assert mask.sub_size == 1
 
-        mask = aa.Mask1D.manual(
-            mask=[False, False, True, True], pixel_scales=3.0, sub_size=2, origin=(1.0,)
-        )
+def test__manual__invert_is_true():
 
-        assert type(mask) == aa.Mask1D
-        assert (mask == np.array([False, False, True, True])).all()
-        assert mask.pixel_scale == 3.0
-        assert mask.origin == (1.0,)
-        assert mask.sub_size == 2
+    mask = aa.Mask1D.manual(mask=[True, True, False], pixel_scales=1.0, invert=True)
 
-        mask = aa.Mask1D.manual(
-            mask=[False, False, True, True, True, False, False, True],
-            pixel_scales=1.0,
-            sub_size=2,
-        )
+    assert type(mask) == aa.Mask1D
+    assert (mask == np.array([False, False, True])).all()
 
-        assert type(mask) == aa.Mask1D
-        assert (
-            mask == np.array([False, False, True, True, True, False, False, True])
-        ).all()
-        assert mask.pixel_scale == 1.0
-        assert mask.origin == (0.0,)
-        assert mask.sub_size == 2
 
-    def test__mask__invert_is_true_inverts_the_mask(self):
+def test__manual__input_is_2d_mask__no_shape_native__raises_exception():
 
-        mask = aa.Mask1D.manual(mask=[True, True, False], pixel_scales=1.0, invert=True)
+    with pytest.raises(exc.MaskException):
 
-        assert type(mask) == aa.Mask1D
-        assert (mask == np.array([False, False, True])).all()
+        aa.Mask1D.manual(mask=[[False, False, True]], pixel_scales=1.0)
 
-    def test__mask__input_is_2d_mask__no_shape_native__raises_exception(self):
 
-        with pytest.raises(exc.MaskException):
+def test__is_all_true():
 
-            aa.Mask1D.manual(mask=[[False, False, True]], pixel_scales=1.0)
+    mask = aa.Mask1D.manual(mask=[False, False, False, False], pixel_scales=1.0)
 
-    def test__is_all_true(self):
+    assert mask.is_all_true is False
 
-        mask = aa.Mask1D.manual(mask=[False, False, False, False], pixel_scales=1.0)
+    mask = aa.Mask1D.manual(mask=[False, False], pixel_scales=1.0)
 
-        assert mask.is_all_true is False
+    assert mask.is_all_true is False
 
-        mask = aa.Mask1D.manual(mask=[False, False], pixel_scales=1.0)
+    mask = aa.Mask1D.manual(mask=[False, True, False, False], pixel_scales=1.0)
 
-        assert mask.is_all_true is False
+    assert mask.is_all_true is False
 
-        mask = aa.Mask1D.manual(mask=[False, True, False, False], pixel_scales=1.0)
+    mask = aa.Mask1D.manual(mask=[True, True, True, True], pixel_scales=1.0)
 
-        assert mask.is_all_true is False
+    assert mask.is_all_true is True
 
-        mask = aa.Mask1D.manual(mask=[True, True, True, True], pixel_scales=1.0)
 
-        assert mask.is_all_true is True
+def test__is_all_false():
 
-    def test__is_all_false(self):
+    mask = aa.Mask1D.manual(mask=[False, False, False, False], pixel_scales=1.0)
 
-        mask = aa.Mask1D.manual(mask=[False, False, False, False], pixel_scales=1.0)
+    assert mask.is_all_false is True
 
-        assert mask.is_all_false is True
+    mask = aa.Mask1D.manual(mask=[False, False], pixel_scales=1.0)
 
-        mask = aa.Mask1D.manual(mask=[False, False], pixel_scales=1.0)
+    assert mask.is_all_false is True
 
-        assert mask.is_all_false is True
+    mask = aa.Mask1D.manual(mask=[False, True, False, False], pixel_scales=1.0)
 
-        mask = aa.Mask1D.manual(mask=[False, True, False, False], pixel_scales=1.0)
+    assert mask.is_all_false is False
 
-        assert mask.is_all_false is False
+    mask = aa.Mask1D.manual(mask=[True, True, False, False], pixel_scales=1.0)
 
-        mask = aa.Mask1D.manual(mask=[True, True, False, False], pixel_scales=1.0)
+    assert mask.is_all_false is False
 
-        assert mask.is_all_false is False
 
-    def test__to_mask_2d(self):
+def test__to_mask_2d():
 
-        mask_1d = aa.Mask1D.manual(mask=[False, True], pixel_scales=1.0, sub_size=2)
+    mask_1d = aa.Mask1D.manual(mask=[False, True], pixel_scales=1.0, sub_size=2)
 
-        mask_2d = mask_1d.to_mask_2d
+    mask_2d = mask_1d.to_mask_2d
 
-        assert (mask_2d == np.array([[False, True]])).all()
-        assert mask_2d.pixel_scales == (1.0, 1.0)
-        assert mask_2d.sub_size == 2
-        assert mask_2d.origin == (0.0, 0.0)
+    assert (mask_2d == np.array([[False, True]])).all()
+    assert mask_2d.pixel_scales == (1.0, 1.0)
+    assert mask_2d.sub_size == 2
+    assert mask_2d.origin == (0.0, 0.0)
 
-    def test__unmasked_mask(self):
 
-        mask = aa.Mask1D.manual(
-            mask=[True, False, True, False, False, False, True, False, True],
-            pixel_scales=1.0,
-        )
+def test__unmasked_mask():
 
-        assert (mask.unmasked_mask == np.full(fill_value=False, shape=(9,))).all()
+    mask = aa.Mask1D.manual(
+        mask=[True, False, True, False, False, False, True, False, True],
+        pixel_scales=1.0,
+    )
+
+    assert (mask.unmasked_mask == np.full(fill_value=False, shape=(9,))).all()
