776c4d13c0342c895300b814ae8c971fec617cb1
Richard <richardhayes777@gmail.com>
2023-02-27 11:14:52 +0000

    remove need for reduce

--- autoarray/abstract_ndarray.py
+++ autoarray/abstract_ndarray.py
@@ -53,24 +53,6 @@ class AbstractNDArray(ABC):
     def __invert__(self):
         return ~self._array
 
-    def __reduce__(self):
-
-        pickled_state = super().__reduce__()
-
-        class_dict = {}
-        for key, value in self.__dict__.items():
-            class_dict[key] = value
-        new_state = pickled_state[2] + (class_dict,)
-
-        return pickled_state[0], pickled_state[1], new_state
-
-    # noinspection PyMethodOverriding
-    def __setstate__(self, state):
-
-        for key, value in state[-1].items():
-            setattr(self, key, value)
-        self._array.__setstate__(state[0:-1])
-
     @property
     @abstractmethod
     def native(self) -> Structure:
