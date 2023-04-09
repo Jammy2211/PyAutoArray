1b5bbc1dad152463136c5d0619c8b80a27d7c546
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-04-09 21:39:03 +0100

    try and fail to optimise but its a little faster

--- autoarray/util/cholesky_funcs.py
+++ autoarray/util/cholesky_funcs.py
@@ -1,70 +1,11 @@
 import numpy as np
 from scipy import linalg
 import math
-import numba
-
-"""
-    Cholesky related functions that might be helpful for building the Cholesky scheme for fnnls.
-    For the scheme currently used, we only use four functions listed here:
-        * _cholupdate (might be replaced by _numba_cholupdate for speed up)
-        * choldeleteindex 
-        * choldeleteindexes
-        * cholinsertlast
-    Other possible schemes may use other functions, so we leave them here for future development.
-"""
+import time
+from numba import njit
 
 
-def cholupdate(U, x, lower=False):
-    if lower:
-        return cholupdate(np.transpose(U), x).T
-
-    U = np.array(U)
-    x = np.array(x)
-    return _cholupdate(U, x)
-
-
-@numba.jit(nopython=True)
-def _numba_cholupdate(U, x, sgn=1):
-    n = x.size
-    for k in range(n):
-        Ukk = U[k, k]
-        xk = x[k]
-        r = math.sqrt(Ukk**2 + sgn * xk**2)
-        c = r / Ukk
-        s = xk / Ukk
-        U[k, k] = r
-        for j in range(k + 1, n):
-            U[k, j] = (U[k, j] + sgn * s * x[j]) / c
-            x[j] = c * x[j] - s * U[k, j]
-    return U
-
-
-def _cholupdate(U, x):
-    n = x.size
-    for k in range(n - 1):
-        Ukk = U[k, k]
-        xk = x[k]
-        r = math.sqrt(Ukk**2 + xk**2)
-        c = r / Ukk
-        s = xk / Ukk
-        U[k, k] = r
-        U[k, k + 1 :] = (U[k, (k + 1) :] + s * x[k + 1 :]) / c
-        x[k + 1 :] = c * x[k + 1 :] - s * U[k, k + 1 :]
-
-    k = n - 1
-    U[k, k] = math.sqrt(U[k, k] ** 2 + x[k] ** 2)
-    return U
-
-
-def choldowndate(U, x, lower=False):
-    if lower:
-        return choldowndate(np.transpose(U), x).T
-
-    U = np.array(U)
-    x = np.array(x)
-    return _choldowndate(U, x)
-
-
+@njit(fastmath=True)
 def _choldowndate(U, x):
     n = x.size
     for k in range(n - 1):
@@ -81,40 +22,40 @@ def _choldowndate(U, x):
     U[k, k] = math.sqrt(U[k, k] ** 2 - x[k] ** 2)
     return U
 
+@njit(fastmath=True)
+def _cholupdate(U, x):
 
-def choldeleteindex(U, index):
-    """
-    This function is called in the `fix_constraint_Cholesky` of fnnls.
-    Although we have _numba_cholupdate here, but we doesn't use it as we doesn't gain much speed
-        up for my tests (1000 source pixels).
-    """
-    s23 = U[index, index + 1 :]
-    L = np.delete(np.delete(U, index, axis=0), index, axis=1)
-    if len(s23) == 0:
-        # If the deleted index is at the end of matrix, then we do not need to update the U.
-        return L
-    else:
-        _cholupdate(L[index:, index:], s23)
-        return L
+    n = x.size
+    for k in range(n - 1):
 
+        Ukk = U[k, k]
+        xk = x[k]
 
-def choldeleteindexes(U, indexes):
-    indexes = sorted(indexes, reverse=True)
-    for i in indexes:
-        U = choldeleteindex(U, i)
+        r = np.sqrt(Ukk**2 + xk**2)
+
+        c = r / Ukk
+        s = xk / Ukk
+        U[k, k] = r
+
+        U[k, k + 1 :] = (U[k, (k + 1) :] + s * x[k + 1 :]) / c
+        x[k + 1 :] = c * x[k + 1 :] - s * U[k, k + 1 :]
+
+    k = n - 1
+    U[k, k] = np.sqrt(U[k, k] ** 2 + x[k] ** 2)
 
     return U
 
 
-def cholinsert(U, index, x, lower=False):
-    if lower:
-        return cholinsert(np.transpose(U), index, x).T
+def cholinsert(U, index, x):
 
     S = np.insert(np.insert(U, index, 0, axis=0), index, 0, axis=1)
+
     S[:index, index] = S12 = linalg.solve_triangular(
-        U[:index, :index], x[:index], trans=1, lower=False
+        U[:index, :index], x[:index], trans=1, lower=False, overwrite_b=True
     )
+
     S[index, index] = s22 = math.sqrt(x[index] - S12.dot(S12))
+
     if index == U.shape[0]:
         return S
     else:
@@ -123,7 +64,7 @@ def cholinsert(U, index, x, lower=False):
         return S
 
 
-def cholinsertlast(U, x, lower=False):
+def cholinsertlast(U, x):
     """
     Update the Cholesky matrix U by inserting a vector at the end of the matrix
     Inserting a vector to the end of U doesn't require _cholupdate, so save some time.
@@ -132,11 +73,32 @@ def cholinsertlast(U, x, lower=False):
         separate it out from the `cholinsert`.
     """
     index = U.shape[0]
-    if lower:
-        return cholinsert(np.transpose(U), index, x).T
+
     S = np.insert(np.insert(U, index, 0, axis=0), index, 0, axis=1)
+
     S[:index, index] = S12 = linalg.solve_triangular(
-        U[:index, :index], x[:index], trans=1, lower=False
+        U[:index, :index], x[:index], trans=1, lower=False, overwrite_b=True
     )
+
     S[index, index] = s22 = math.sqrt(x[index] - S12.dot(S12))
+
     return S
+
+
+def choldeleteindexes(U, indexes):
+
+    indexes = sorted(indexes, reverse=True)
+
+    for index in indexes:
+
+        L = np.delete(np.delete(U, index, axis=0), index, axis=1)
+
+        # If the deleted index is at the end of matrix, then we do not need to update the U.
+
+        if index == L.shape[0]:
+            U = L
+        else:
+            _cholupdate(L[index:, index:], U[index, index + 1:])
+            U = L
+
+    return U
--- autoarray/util/fnnls.py
+++ autoarray/util/fnnls.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import linalg as slg
+
 from autoarray.util.cholesky_funcs import cholinsertlast, choldeleteindexes
 
 """
@@ -14,370 +15,6 @@ from autoarray.util.cholesky_funcs import cholinsertlast, choldeleteindexes
         will speed up our task (~ 1000 free parameters) by ~ 3 times as it significantly reduces the
         iteration time.
 """
-
-
-def fnnls(
-    Z,
-    x,
-    P_initial=np.zeros(0, dtype=int),
-    lstsq=lambda A, x: slg.solve(A, x, assume_a="pos"),
-):
-    """
-    Implementation of the Fast Non-megative Least Squares Algorithm described
-    in the paper "A fast non-negativity-constrained least squares algorithm"
-    by Rasmus Bro and Sijmen De Jong.
-
-    This algorithm seeks to find min_d ||x - Zd|| subject to d >= 0
-
-    Some of the comments, such as "B2", refer directly to the steps of
-    the fnnls algorithm as presented in the paper by Bro et al.
-
-    Parameters
-    ----------
-    Z: NumPy array
-        Z is an m x n matrix.
-
-    x: Numpy array
-        x is a m x 1 vector.
-
-    P_initial: Numpy array, dtype=int
-        By default, an empty array. An estimate for
-        the indices of the support of the solution.
-
-        lstsq: function
-        By default, numpy.linalg.lstsq with rcond=None.
-        Least squares function to use when calculating the
-        least squares solution min_x ||Ax - b||.
-        Must be of the form x = f(A,b).
-
-    Returns
-    -------
-    d: Numpy array
-        d is a nx1 vector
-    """
-
-    Z, x, P_initial = map(np.asarray_chkfinite, (Z, x, P_initial))
-
-    m, n = Z.shape
-
-    if len(Z.shape) != 2:
-        raise ValueError(
-            "Expected a two-dimensional array, but Z is of shape {}".format(Z.shape)
-        )
-    if len(x.shape) != 1:
-        raise ValueError(
-            "Expected a one-dimensional array, but x is of shape {}".format(x.shape)
-        )
-    if len(P_initial.shape) != 1:
-        raise ValueError(
-            "Expected a one-dimensional array, but P_initial is of shape {}".format(
-                P_initial.shape
-            )
-        )
-
-    if not np.all((P_initial - P_initial.astype(int)) == 0):
-        raise ValueError(
-            "Expected only integer values, but P_initial has values {}".format(
-                P_initial[(P_initial - P_initial.astype(int)) != 0]
-            )
-        )
-    if np.any(P_initial >= n):
-        raise ValueError(
-            "Expected values between 0 and Z.shape[1], but P_initial has max value {}".format(
-                np.max(P_initial)
-            )
-        )
-    if np.any(P_initial < 0):
-        raise ValueError(
-            "Expected values between 0 and Z.shape[1], but P_initial has min value {}".format(
-                np.min(P_initial)
-            )
-        )
-    if P_initial.dtype != np.dtype("int64") and P_initial.dtype != np.dtype("int32"):
-        raise TypeError(
-            "Expected type int64 or int32, but P_initial is type {}".format(
-                P_initial.dtype
-            )
-        )
-    if x.shape[0] != m:
-        raise ValueError(
-            "Incompatable dimensions. The first dimension of Z should match the length of x, but Z is of shape {} and x is of shape {}".format(
-                Z.shape, x.shape
-            )
-        )
-
-    # Calculating ZTZ and ZTx in advance to improve the efficiency of calculations
-    ZTZ = Z.T.dot(Z)
-    ZTx = Z.T.dot(x)
-
-    # Declaring constants for tolerance and max repetitions
-    epsilon = 2.2204e-16
-    tolerance = epsilon * n
-
-    # number of contsecutive times the set P can remain unchanged loop until we terminate
-    max_repetitions = 5
-
-    # A1 + A2
-    P = np.zeros(n, dtype=np.bool)
-    P[P_initial] = True
-
-    # A3
-    d = np.zeros(n)
-
-    # A4
-    w = ZTx - (ZTZ) @ d
-
-    # Initialize s
-    s = np.zeros(n)
-
-    # Count of amount of consecutive times set P has remained unchanged
-    no_update = 0
-
-    # Extra loop in case a support is set to update s and d
-    if P_initial.shape[0] != 0:
-
-        s[P] = lstsq((ZTZ)[P][:, P], (ZTx)[P])
-        d = s.clip(min=0)
-
-    # B1
-    while (not np.all(P)) and np.max(w[~P]) > tolerance:
-
-        current_P = (
-            P.copy()
-        )  # make copy of passive set to check for change at end of loop
-
-        # B2 + B3
-        P[np.argmax(w * ~P)] = True
-
-        # B4
-        s[P] = lstsq((ZTZ)[P][:, P], (ZTx)[P])
-
-        # C1
-        while np.any(P) and np.min(s[P]) <= tolerance:
-
-            s, d, P = fix_constraint(ZTZ, ZTx, s, d, P, tolerance, lstsq)
-
-        # B5
-        d = s.copy()
-        # B6
-        w = ZTx - (ZTZ) @ d
-
-        # check if there has been a change to the passive set
-        if np.all(current_P == P):
-            no_update += 1
-        else:
-            no_update = 0
-
-        if no_update >= max_repetitions:
-            break
-
-    res = np.linalg.norm(x - Z @ d)  # Calculate residual loss ||x - Zd||
-
-    return [d, res]
-
-
-def fnnls_modified(
-    ZTZ,
-    ZTx,
-    P_initial=np.zeros(0, dtype=int),
-    lstsq=lambda A, x: slg.solve(A, x, assume_a="pos"),
-):
-    """
-    Implementation of the Fast Non-megative Least Squares Algorithm described
-    in the paper "A fast non-negativity-constrained least squares algorithm"
-    by Rasmus Bro and Sijmen De Jong.
-
-    This algorithm seeks to find min_d ||x - Zd|| subject to d >= 0
-
-    Some of the comments, such as "B2", refer directly to the steps of
-    the fnnls algorithm as presented in the paper by Bro et al.
-
-    Parameters
-    ----------
-    Z: NumPy array
-        Z is an m x n matrix.
-
-    x: Numpy array
-        x is a m x 1 vector.
-
-    P_initial: Numpy array, dtype=int
-        By default, an empty array. An estimate for
-        the indices of the support of the solution.
-
-        lstsq: function
-        By default, numpy.linalg.lstsq with rcond=None.
-        Least squares function to use when calculating the
-        least squares solution min_x ||Ax - b||.
-        Must be of the form x = f(A,b).
-
-    Returns
-    -------
-    d: Numpy array
-        d is a nx1 vector
-    """
-
-    # Z, x, P_initial = map(np.asarray_chkfinite, (Z, x, P_initial))
-
-    n = np.shape(ZTZ)[0]
-
-    # Calculating ZTZ and ZTx in advance to improve the efficiency of calculations
-    # ZTZ = Z.T.dot(Z)
-    # ZTx = Z.T.dot(x)
-
-    # Declaring constants for tolerance and max repetitions
-    epsilon = 2.2204e-16
-    tolerance = epsilon * n
-
-    # number of contsecutive times the set P can remain unchanged loop until we terminate
-    max_repetitions = 2
-
-    # A1 + A2
-    P = np.zeros(n, dtype=np.bool)
-    P[P_initial] = True
-
-    # A3
-    d = np.zeros(n)
-
-    # A4
-    w = ZTx - (ZTZ) @ d
-
-    # Initialize s
-    s = np.zeros(n)
-
-    # Count of amount of consecutive times set P has remained unchanged
-    no_update = 0
-
-    #   count = 0
-    #    sub_count = 0
-
-    # Extra loop in case a support is set to update s and d
-    if P_initial.shape[0] != 0:
-
-        s[P] = lstsq((ZTZ)[P][:, P], (ZTx)[P])
-        d = s.clip(min=0)
-
-    # B1
-    while (not np.all(P)) and np.max(w[~P]) > tolerance:
-
-        #  count += 1
-
-        current_P = (
-            P.copy()
-        )  # make copy of passive set to check for change at end of loop
-
-        # B2 + B3
-        P[np.argmax(w * ~P)] = True
-
-        # B4
-        s[P] = lstsq((ZTZ)[P][:, P], (ZTx)[P])
-
-        # C1
-        while np.any(P) and np.min(s[P]) <= tolerance:
-
-            #      sub_count += 1
-
-            s, d, P = fix_constraint(ZTZ, ZTx, s, d, P, tolerance, lstsq)
-
-        # B5
-        d = s.copy()
-        # B6
-        w = ZTx - (ZTZ) @ d
-
-        # check if there has been a change to the passive set
-        if np.all(current_P == P):
-            no_update += 1
-        else:
-            no_update = 0
-
-        if no_update >= max_repetitions:
-            break
-
-    # res = np.linalg.norm(x - Z@d) #Calculate residual loss ||x - Zd||
-
-    # print(f"Total Iterations = {count} / {sub_count}")
-
-    return d
-
-
-def fix_constraint(
-    ZTZ,
-    ZTx,
-    s,
-    d,
-    P,
-    tolerance,
-    lstsq=lambda A, x: slg.solve(A, x, assume_a="pos"),
-):
-    """
-    The inner loop of the Fast Non-megative Least Squares Algorithm described
-    in the paper "A fast non-negativity-constrained least squares algorithm"
-    by Rasmus Bro and Sijmen De Jong.
-
-    One iteration of the loop to adjust the new estimate s to satisfy the
-    nonnegativity contraint of the solution.
-
-    Some of the comments, such as "B2", refer directly to the steps of
-    the fnnls algorithm as presented in the paper by Bro et al.
-
-    Parameters
-    ----------
-    ZTZ: NumPy array
-        ZTZ is an n x n matrix equal to Z.T * Z
-
-    ZTx: Numpy array
-        ZTx is an n x 1 vector equal to Z.T * x
-
-    s: Numpy array
-        The new estimate of the solution with possible
-        negative values that do not meet the constraint
-
-    d: Numpy array
-        The previous estimate of the solution that satisfies
-        the nonnegativity contraint
-
-    P: Numpy array, dtype=np.bool
-        The current passive set, which comtains the indices
-        that are not fixed at the value zero.
-
-    tolerance: float
-        A tolerance, below which values are considered to be
-        0, allowing for more reasonable convergence.
-
-    lstsq: function
-        By default, numpy.linalg.lstsq with rcond=None.
-        Least squares function to use when calculating the
-        least squares solution min_x ||Ax - b||.
-        Must be of the form x = f(A,b).
-
-    Returns
-    -------
-    s: Numpy array
-        The updated new estimate of the solution.
-    d: Numpy array
-        The updated previous estimate, now as close
-        as possible to s while maintaining nonnegativity.
-    P: Numpy array, dtype=np.bool
-        The updated passive set
-    """
-    # C2
-    q = P * (s <= tolerance)
-    alpha = np.min(d[q] / (d[q] - s[q]))
-
-    # C3
-    d = d + alpha * (
-        s - d
-    )  # set d as close to s as possible while maintaining non-negativity
-
-    # C4
-    P[d <= tolerance] = False
-
-    # C5
-    s[P] = lstsq((ZTZ)[P][:, P], (ZTx)[P])
-
-    # C6
-    s[~P] = 0.0
-
-    return s, d, P
 
 
 def fnnls_cholesky(
@@ -400,7 +37,7 @@ def fnnls_cholesky(
     n = np.shape(ZTZ)[0]
     epsilon = 2.2204e-16
     tolerance = epsilon * n
-    max_repetitions = 5
+    max_repetitions = 3
     no_update = 0
     loop_count = 0
 
@@ -426,22 +63,28 @@ def fnnls_cholesky(
     #     and can sort them in the order of added to the passive set. This will make updating the
     #     Cholesky factorisation simpler and thus save time.
 
+
     while (not np.all(P)) and np.max(w[~P]) > tolerance:
-        current_P = P.copy()
+
         # make copy of passive set to check for change at end of loop
+
+        current_P = P.copy()
         idmax = np.argmax(w * ~P)
         P_inorder = np.append(P_inorder, int(idmax))
+
         if loop_count == 0:
-            U = slg.cholesky(ZTZ[P_inorder][:, P_inorder])
             # We need to initialize the Cholesky factorisation, U, for the first loop.
+            U = slg.cholesky(ZTZ[P_inorder][:, P_inorder])
         else:
             U = cholinsertlast(U, ZTZ[idmax][P_inorder])
-        s_chol[P_inorder] = slg.cho_solve((U, False), ZTx[P_inorder])
+
         # solve the lstsq problem by cho_solve
+
+        s_chol[P_inorder] = slg.cho_solve((U, False), ZTx[P_inorder])
+
         P[idmax] = True
         while np.any(P) and np.min(s_chol[P]) <= tolerance:
             s_chol, d, P, P_inorder, U = fix_constraint_cholesky(
-                ZTZ=ZTZ,
                 ZTx=ZTx,
                 s_chol=s_chol,
                 d=d,
@@ -458,13 +101,14 @@ def fnnls_cholesky(
             no_update += 1
         else:
             no_update = 0
+
         if no_update >= max_repetitions:
             break
 
     return d
 
 
-def fix_constraint_cholesky(ZTZ, ZTx, s_chol, d, P, P_inorder, U, tolerance):
+def fix_constraint_cholesky(ZTx, s_chol, d, P, P_inorder, U, tolerance):
     """
     Similar to fix_constraint, but solve the lstsq by Cholesky factorisation.
     If this function is called, it means some solutions in the current passive sets needed to be
@@ -479,16 +123,22 @@ def fix_constraint_cholesky(ZTZ, ZTx, s_chol, d, P, P_inorder, U, tolerance):
     """
     q = P * (s_chol <= tolerance)
     alpha = np.min(d[q] / (d[q] - s_chol[q]))
+
+    # set d as close to s as possible while maintaining non-negativity
     d = d + alpha * (
         s_chol - d
-    )  # set d as close to s as possible while maintaining non-negativity
+    )
 
     id_delete = np.where(d[P_inorder] <= tolerance)[0]
+
     U = choldeleteindexes(U, id_delete)  # update the Cholesky factorisation
+
     P_inorder = np.delete(P_inorder, id_delete)  # update the P_inorder
+
     P[d <= tolerance] = False  # update the P
 
     # solve the lstsq problem by cho_solve
+
     s_chol[P_inorder] = slg.cho_solve((U, False), ZTx[P_inorder])
 
     s_chol[~P] = 0.0  # set solutions taken out of the passive set to be 0
