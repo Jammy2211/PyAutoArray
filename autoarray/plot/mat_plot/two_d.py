ce3519250965e0d9f33c69da3a85d0494f4333a8
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-10-23 13:57:02 +0100

    uncomment contour plot

--- autoarray/plot/mat_plot/two_d.py
+++ autoarray/plot/mat_plot/two_d.py
@@ -338,14 +338,14 @@ class MatPlot2D(AbstractMatPlot):
             )
             self.colorbar_tickparams.set(cb=cb)
 
-        levels = np.logspace(np.log10(0.3), np.log10(20.0), 10)
-        plt.contour(
-            #  array.mask.derive_grid.unmasked_sub_1,
-            array.native[::-1],
-            levels=levels,
-            colors="black",
-            extent=extent,
-        )
+        # levels = np.logspace(np.log10(0.3), np.log10(20.0), 10)
+        # plt.contour(
+        #     #  array.mask.derive_grid.unmasked_sub_1,
+        #     array.native[::-1],
+        #     levels=levels,
+        #     colors="black",
+        #     extent=extent,
+        # )
 
         grid_indexes = None
 
