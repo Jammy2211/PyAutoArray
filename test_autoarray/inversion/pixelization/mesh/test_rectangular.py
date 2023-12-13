265c96903ae72e2f221cf4f98a56f874afb3f4a9
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-12-13 15:26:15 +0000

    clean up pixeliation.mesh.test_rectangular

--- test_autoarray/inversion/pixelization/mesh/test_rectangular.py
+++ test_autoarray/inversion/pixelization/mesh/test_rectangular.py
@@ -3,21 +3,12 @@ import pytest
 import autoarray as aa
 
 
-def test__sparse_grid_from__returns_none_as_not_used(sub_grid_2d_7x7):
-    pixelization = aa.mesh.Rectangular(shape=(3, 3))
-
-    assert (
-        pixelization.image_plane_mesh_grid_from(image_plane_data_grid=sub_grid_2d_7x7)
-        == None
-    )
-
-
 def test__preloads_used_for_relocated_grid(sub_grid_2d_7x7):
-    pixelization = aa.mesh.Rectangular(shape=(3, 3))
+    mesh = aa.mesh.Rectangular(shape=(3, 3))
 
     relocated_grid = aa.Grid2D.uniform(shape_native=(3, 3), pixel_scales=1.0)
 
-    mapper_grids = pixelization.mapper_grids_from(
+    mapper_grids = mesh.mapper_grids_from(
         source_plane_data_grid=relocated_grid,
         source_plane_mesh_grid=None,
         settings=aa.SettingsPixelization(use_border=True),
