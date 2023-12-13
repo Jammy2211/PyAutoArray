3db2e5b0a8804e16c14197c9ca969b9ad2e0d56f
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-12-13 13:52:15 +0000

    remove SNRSplit class

--- autoarray/inversion/pixelization/mesh/__init__.py
+++ autoarray/inversion/pixelization/mesh/__init__.py
@@ -2,9 +2,7 @@ from .abstract import AbstractMesh as Mesh
 from .rectangular import Rectangular
 from .voronoi import VoronoiMagnification
 from .voronoi import VoronoiBrightnessImage
-from .voronoi import VoronoiSNRImage
 from .voronoi_nn import VoronoiNNMagnification
 from .voronoi_nn import VoronoiNNBrightnessImage
-from .voronoi_nn import VoronoiNNSNRImage
 from .delaunay import DelaunayMagnification
 from .delaunay import DelaunayBrightnessImage
