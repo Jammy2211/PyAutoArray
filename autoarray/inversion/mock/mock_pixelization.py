732d145b7e45e44f20cb95117d36cd9037e8a9c9
James Nightingale <james.w.nightingale@durham.ac.uk>
2023-12-14 09:36:04 +0000

    fix image mesh in mock objects

--- autoarray/inversion/mock/mock_pixelization.py
+++ autoarray/inversion/mock/mock_pixelization.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from autoarray.inversion.pixelization.image_mesh.abstract import AbstractImageMesh
 from autoarray.inversion.pixelization.pixelization import Pixelization
 
 
@@ -12,6 +13,9 @@ class MockPixelization(Pixelization):
         mapper=None,
         image_plane_mesh_grid=None,
     ):
+
+        image_mesh = image_mesh or AbstractImageMesh
+
         super().__init__(
             mesh=mesh, regularization=regularization, image_mesh=image_mesh
         )
