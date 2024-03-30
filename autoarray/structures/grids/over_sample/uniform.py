5a316e43e439143ac5f0f9fc71fd004ce9e6a3fd
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-03-30 10:46:43 +0000

    remove sub_size from indexes derive

--- autoarray/structures/grids/over_sample/uniform.py
+++ autoarray/structures/grids/over_sample/uniform.py
@@ -26,6 +26,8 @@ class OverSampleUniform(AbstractOverSample):
         significantly within a pixel. To achieve precision in these pixels a high `sub_size` is required, which can
         be computationally expensive as it is applied to every pixel.
 
+        - ``native_for_slim``: returns an array of shape [total_unmasked_pixels*sub_size] that
+        maps every unmasked sub-pixel to its corresponding native 2D pixel using its (y,x) pixel indexes.
 
         **Case 2 (sub-size>1, slim)**
 
@@ -186,6 +188,168 @@ class OverSampleUniform(AbstractOverSample):
             mask=self.mask,
             over_sample=self.over_sample,
         )
+
+    @property
+    def native_for_slim(self) -> np.ndarray:
+        """
+        Derives a 1D ``ndarray`` which maps every non-subgridded 1D ``slim`` index of the ``Mask2D`` to its
+        non-subgridded 2D ``native`` index.
+
+        For example, for the following ``Mask2D``:
+
+        ::
+            [[True,  True,  True, True]
+             [True, False, False, True],
+             [True, False,  True, True],
+             [True,  True,  True, True]]
+
+        This has three unmasked (``False`` values) which have the ``slim`` indexes:
+
+        ::
+            [0, 1, 2]
+
+        The array ``native_for_slim`` is therefore:
+
+        ::
+            [[1,1], [1,2], [2,1]]
+
+        For `sub_size=2`` each unmasked ``False`` entry is split into a sub-pixel of size 2x2.
+        However, this method ignores sub-gridding and therefore will still produce the same arrays above, as if
+        ``sub_size=1``.
+
+        Examples
+        --------
+
+        .. code-block:: python
+
+            import autoarray as aa
+
+            mask_2d = aa.Mask2D(
+                mask=[[True,  True,  True, True]
+                      [True, False, False, True],
+                      [True, False,  True, True],
+                      [True,  True,  True, True]]
+                pixel_scales=1.0,
+            )
+
+            derive_indexes_2d = aa.DeriveIndexes2D(mask=mask_2d)
+
+            print(derive_indexes_2d.native_for_slim)
+        """
+        return mask_2d_util.native_index_for_slim_index_2d_from(
+            mask_2d=np.array(self.mask), sub_size=1
+        ).astype("int")
+
+    @cached_property
+    def sub_mask_native_for_sub_mask_slim(self) -> np.ndarray:
+        """
+        Derives a 1D ``ndarray`` which maps every subgridded 1D ``slim`` index of the ``Mask2D`` to its
+        subgridded 2D ``native`` index.
+
+        For example, for the following ``Mask2D`` for ``sub_size=1``:
+
+        ::
+            [[True,  True,  True, True]
+             [True, False, False, True],
+             [True, False,  True, True],
+             [True,  True,  True, True]]
+
+        This has three unmasked (``False`` values) which have the ``slim`` indexes:
+
+        ::
+            [0, 1, 2]
+
+        The array ``sub_mask_native_for_sub_mask_slim`` is therefore:
+
+        ::
+            [[1,1], [1,2], [2,1]]
+
+        For a ``Mask2D`` with ``sub_size=2`` each unmasked ``False`` entry is split into a sub-pixel of size 2x2 and
+        there are therefore 12 ``slim`` indexes:
+
+        ::
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+        The array ``native_for_slim`` is therefore:
+
+        ::
+            [[2,2], [2,3], [2,4], [2,5], [3,2], [3,3], [3,4], [3,5], [4,2], [4,3], [5,2], [5,3]]
+
+        Examples
+        --------
+
+        .. code-block:: python
+
+            import autoarray as aa
+
+            mask_2d = aa.Mask2D(
+                mask=[[True,  True,  True, True]
+                      [True, False, False, True],
+                      [True, False,  True, True],
+                      [True,  True,  True, True]]
+                pixel_scales=1.0,
+            )
+
+            derive_indexes_2d = aa.DeriveIndexes2D(mask=mask_2d)
+
+            print(derive_indexes_2d.sub_mask_native_for_sub_mask_slim)
+        """
+        return mask_2d_util.native_index_for_slim_index_2d_from(
+            mask_2d=self.mask.array, sub_size=self.mask.sub_size
+        ).astype("int")
+
+    @cached_property
+    def slim_for_sub_slim(self) -> np.ndarray:
+        """
+        Derives a 1D ``ndarray`` which maps every subgridded 1D ``slim`` index of the ``Mask2D`` to its
+        non-subgridded 1D ``slim`` index.
+
+        For example, for the following ``Mask2D`` for ``sub_size=1``:
+
+        ::
+            [[True,  True,  True, True]
+             [True, False, False, True],
+             [True, False,  True, True],
+             [True,  True,  True, True]]
+
+        This has three unmasked (``False`` values) which have the ``slim`` indexes:
+
+        ::
+            [0, 1, 2]
+
+        The array ``slim_for_sub_slim`` is therefore:
+
+        ::
+            [0, 1, 2]
+
+        For a ``Mask2D`` with ``sub_size=2`` each unmasked ``False`` entry is split into a sub-pixel of size 2x2.
+        Therefore the array ``slim_for_sub_slim`` becomes:
+
+        ::
+            [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2]
+
+        Examples
+        --------
+
+        .. code-block:: python
+
+            import autoarray as aa
+
+            mask_2d = aa.Mask2D(
+                mask=[[True,  True,  True, True]
+                      [True, False, False, True],
+                      [True, False,  True, True],
+                      [True,  True,  True, True]]
+                pixel_scales=1.0,
+            )
+
+            derive_indexes_2d = aa.DeriveIndexes2D(mask=mask_2d)
+
+            print(derive_indexes_2d.slim_for_sub_slim)
+        """
+        return mask_2d_util.slim_index_for_sub_slim_index_via_mask_2d_from(
+            mask_2d=np.array(self.mask), sub_size=self.mask.sub_size
+        ).astype("int")
 
     def structure_2d_from(self, result: np.ndarray, mask : Mask2D) -> Union[Array2D, "Grid2D"]:
         """
