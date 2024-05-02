0f2b276c7e6cf2c1b2f19d9000210f1c6c95fd63
James Nightingale <james.w.nightingale@durham.ac.uk>
2024-05-02 17:35:38 +0100

    black

--- autoarray/dataset/mock/mock_dataset.py
+++ autoarray/dataset/mock/mock_dataset.py
@@ -1,5 +1,10 @@
+from autoarray.structures.grids.uniform_2d import Grid2D
+
+
 class MockDataset:
     def __init__(self, grid_pixelization=None, psf=None, mask=None):
-        self.grid_pixelization = grid_pixelization
+        self.grid_pixelization = grid_pixelization or Grid2D.no_mask(
+            values=[[[1.0, 1.0]]], pixel_scales=1.0
+        )
         self.psf = psf
         self.mask = mask
