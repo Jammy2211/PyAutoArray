ae56678d31df32109d15b2021ff76c921c259121
Richard <richard@rghsoftware.co.uk>
2024-05-13 17:13:23 +0100

    fix test

--- test_autoarray/structures/triangles/test_update_triangles.py
+++ test_autoarray/structures/triangles/test_update_triangles.py
@@ -7,10 +7,7 @@ def test_update(triangles):
     transformed = 2 * triangles.grid_2d
     new = triangles.with_updated_grid(grid=transformed)
 
-    assert len(new.rows) == len(triangles.rows)
-    assert len(new.grid_2d) == len(triangles.grid_2d)
-    assert (new.grid_2d == transformed).all()
-    assert len(new.triangles) == 15
+    assert len(new) == len(triangles.triangles)
 
 
 @pytest.mark.parametrize(
