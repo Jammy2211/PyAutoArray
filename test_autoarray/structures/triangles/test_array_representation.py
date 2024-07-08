4f416e45af715a809a96d0d65fcbfd293f342517
Richard <richard@rghsoftware.co.uk>
2024-07-08 11:57:38 +0100

    thorough testing of contains

--- test_autoarray/structures/triangles/test_array_representation.py
+++ test_autoarray/structures/triangles/test_array_representation.py
@@ -1,10 +1,12 @@
 import numpy as np
+import pytest
 
 from autoarray.structures.triangles.array import ArrayTriangles
 
 
-def test_contains():
-    triangles = ArrayTriangles(
+@pytest.fixture
+def triangles():
+    return ArrayTriangles(
         indices=np.array(
             [
                 [0, 1, 2],
@@ -21,23 +23,66 @@ def test_contains():
         ),
     )
 
-    containing = triangles.containing((0.1, 0.1))
 
-    assert (
-        containing.indices
-        == np.array(
-            [
-                [0, 2, 1],
-            ]
-        )
-    ).all()
-    assert (
-        containing.vertices
-        == np.array(
-            [
-                [0.0, 0.0],
-                [0.0, 1.0],
-                [1.0, 0.0],
-            ]
-        )
-    ).all()
+@pytest.mark.parametrize(
+    "point, vertices, indices",
+    [
+        (
+            (0.1, 0.1),
+            np.array(
+                [
+                    [0.0, 0.0],
+                    [0.0, 1.0],
+                    [1.0, 0.0],
+                ]
+            ),
+            np.array(
+                [
+                    [0, 2, 1],
+                ]
+            ),
+        ),
+        (
+            (0.6, 0.6),
+            np.array(
+                [
+                    [0.0, 1.0],
+                    [1.0, 0.0],
+                    [1.0, 1.0],
+                ]
+            ),
+            np.array(
+                [
+                    [1, 0, 2],
+                ]
+            ),
+        ),
+        (
+            (0.5, 0.5),
+            np.array(
+                [
+                    [0.0, 0.0],
+                    [0.0, 1.0],
+                    [1.0, 0.0],
+                    [1.0, 1.0],
+                ]
+            ),
+            np.array(
+                [
+                    [0, 2, 1],
+                    [2, 1, 3],
+                ]
+            ),
+        ),
+    ],
+)
+def test_contains_vertices(
+    triangles,
+    point,
+    vertices,
+    indices,
+):
+    containing = triangles.containing(point)
+
+    assert (containing.indices == indices).all()
+    assert (containing.vertices == vertices).all()
