ddffba08e5250d7cda5e4c357ece17065f84831a
Richard <richard@rghsoftware.co.uk>
2024-07-22 14:49:29 +0100

    Revert "use nan as placeholder"
    This reverts commit 677f6a0d79d9991b0e2879d539fe29ffeb5775a0.

--- test_autoarray/structures/triangles/test_jax.py
+++ test_autoarray/structures/triangles/test_jax.py
@@ -71,7 +71,7 @@ def test_contains_vertices(
 ):
     containing_indices = triangles.containing_indices(point)
 
-    assert (np.nan_to_num(containing_indices, nan=-1) == indices).all()
+    assert (containing_indices == indices).all()
 
 
 @pytest.mark.parametrize(
