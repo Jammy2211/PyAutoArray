372fe3186f02a204ddcfaaf3ca61045ad77b917d
Richard <richard@rghsoftware.co.uk>
2024-09-13 12:31:02 +0100

    implementation for triangles and polygon

--- autoarray/structures/triangles/shape.py
+++ autoarray/structures/triangles/shape.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import List, Tuple
 
 import numpy as np
 
@@ -85,6 +86,14 @@ class Point(Shape):
         )
 
 
+def centroid(triangles: np.ndarray):
+    y1, x1 = triangles[:, 0, 1], triangles[:, 0, 0]
+    y2, x2 = triangles[:, 1, 1], triangles[:, 1, 0]
+    y3, x3 = triangles[:, 2, 1], triangles[:, 2, 0]
+
+    return (x1 + x2 + x3) / 3, (y1 + y2 + y3) / 3
+
+
 @register_pytree_node_class
 class Circle(Point):
     def __init__(
@@ -124,13 +133,7 @@ class Circle(Point):
         -------
         A boolean array indicating which triangles intersect the circle.
         """
-        # Get the vertices of the triangles
-        y1, x1 = triangles[:, 0, 1], triangles[:, 0, 0]
-        y2, x2 = triangles[:, 1, 1], triangles[:, 1, 0]
-        y3, x3 = triangles[:, 2, 1], triangles[:, 2, 0]
-
-        centroid_x = (x1 + x2 + x3) / 3
-        centroid_y = (y1 + y2 + y3) / 3
+        centroid_x, centroid_y = centroid(triangles)
 
         a = centroid_x - self.x
         b = centroid_y - self.y
@@ -157,3 +160,138 @@ class Circle(Point):
             y=children[1],
             radius=children[2],
         )
+
+
+class Triangle(Point):
+    def __init__(
+        self,
+        a: Tuple[float, float],
+        b: Tuple[float, float],
+        c: Tuple[float, float],
+    ):
+        """
+        A triangle in the source plane for which we want to identify pixels in the
+        image plane that trace to it.
+
+        Parameters
+        ----------
+        a, b, c
+            The vertices of the triangle.
+        """
+        xs, ys = zip(a, b, c)
+        super().__init__(
+            x=np.mean(xs),
+            y=np.mean(ys),
+        )
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def tree_flatten(self):
+        """
+        Flatten this model as a PyTree.
+        """
+        return (
+            self.a,
+            self.b,
+            self.c,
+        ), None
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        """
+        Unflatten a PyTree into a model.
+        """
+        return cls(
+            *children,
+        )
+
+    def mask(self, triangles: np.ndarray) -> np.ndarray:
+        return self.triangle_contains_mask(triangles) | super().mask(triangles)
+
+    def triangle_contains_mask(self, triangles: np.ndarray) -> np.ndarray:
+        y1, x1 = self.a
+        y2, x2 = self.b
+        y3, x3 = self.c
+
+        denominator = (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3)
+
+        centroid_x, centroid_y = centroid(triangles)
+
+        a = (
+            (y2 - y3) * (centroid_x - x3) + (x3 - x2) * (centroid_y - y3)
+        ) / denominator
+        b = (
+            (y3 - y1) * (centroid_x - x3) + (x1 - x3) * (centroid_y - y3)
+        ) / denominator
+        c = 1 - a - b
+
+        return (0 <= a) & (a <= 1) & (0 <= b) & (b <= 1) & (0 <= c) & (c <= 1)
+
+
+class Polygon(Point):
+    def __init__(
+        self,
+        vertices: List[Tuple[float, float]],
+    ):
+        """
+        A polygon in the source plane for which we want to identify pixels in the
+        image plane that trace to it.
+
+        Parameters
+        ----------
+        vertices
+            The vertices of the polygon.
+        """
+        self.vertices = vertices
+
+        if len(vertices) < 3:
+            raise ValueError("A polygon must have at least 3 vertices.")
+
+        x = np.mean([vertex[0] for vertex in vertices])
+        y = np.mean([vertex[1] for vertex in vertices])
+        super().__init__(x, y)
+
+        first = vertices[0]
+
+        self.triangles = [
+            Triangle(
+                first,
+                second,
+                third,
+            )
+            for second, third in zip(vertices[1:], vertices[2:])
+        ]
+
+    def tree_flatten(self):
+        """
+        Flatten this model as a PyTree.
+        """
+        return (self.vertices,), None
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        """
+        Unflatten a PyTree into a model.
+        """
+        return cls(
+            vertices=children[0],
+        )
+
+    def mask(self, triangles: np.ndarray) -> np.ndarray:
+        """
+        Determine which triangles intersect the Voronoi cell.
+
+        Parameters
+        ----------
+        triangles
+            The vertices of the triangles
+
+        Returns
+        -------
+        A boolean array indicating which triangles intersect the Voronoi cell.
+        """
+        return np.any(
+            [triangle.mask(triangles) for triangle in self.triangles],
+            axis=0,
+        ) | super().mask(triangles)
