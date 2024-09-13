86baf9583bcc19d3ba4bf35043aefd19b3f51ab4
Richard <richard@rghsoftware.co.uk>
2024-09-13 12:42:34 +0100

    test against polygon

--- test_autoarray/structures/triangles/test_voronoi.py
+++ test_autoarray/structures/triangles/test_voronoi.py
@@ -39,3 +39,32 @@ def test_triangle(centroid, is_inside, shape):
             [3 * [centroid]],
         )
     ) == np.array([is_inside])
+
+
+@pytest.mark.parametrize(
+    "centroid, is_inside",
+    [
+        ((0.5, 0.5), True),
+        ((0.0, 0.0), True),
+        ((1.0, 0.0), True),
+        ((0.0, 1.0), True),
+        ((1.0, 1.0), True),
+        ((0.0, 2.0), False),
+        ((2.0, 0.0), False),
+        ((2.0, 2.0), False),
+    ],
+)
+def test_polygon(centroid, is_inside):
+    polygon = Polygon(
+        [
+            (0.0, 0.0),
+            (1.0, 0.0),
+            (1.0, 1.0),
+            (0.0, 1.0),
+        ]
+    )
+    assert polygon.mask(
+        np.array(
+            [3 * [centroid]],
+        )
+    ) == np.array([is_inside])
