368944cf31b334777ce0a2986d915ea5ab0f078c
Richard <richard@rghsoftware.co.uk>
2024-09-13 12:35:07 +0100

    test against triangle

--- test_autoarray/structures/triangles/test_voronoi.py
+++ test_autoarray/structures/triangles/test_voronoi.py
@@ -1,2 +1,29 @@
-def test_voronoi():
-    voronoi = Voronoi()
+import pytest
+
+from autoarray.structures.triangles.shape import Polygon, Triangle
+import numpy as np
+
+
+@pytest.mark.parametrize(
+    "centroid, is_inside",
+    [
+        ((0.5, 0.5), True),
+        ((0.0, 0.0), True),
+        ((1.0, 0.0), True),
+        ((0.0, 1.0), True),
+        ((0.0, 2.0), False),
+        ((2.0, 0.0), False),
+        ((2.0, 2.0), False),
+    ],
+)
+def test_triangle(centroid, is_inside):
+    triangle = Triangle(
+        (0.0, 0.0),
+        (1.0, 0.0),
+        (0.0, 1.0),
+    )
+    assert triangle.mask(
+        np.array(
+            [3 * [centroid]],
+        )
+    ) == np.array([is_inside])
