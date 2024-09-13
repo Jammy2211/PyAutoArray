28fba80e11c5d075386fa26f70aa81f16c7cd512
Richard <richard@rghsoftware.co.uk>
2024-09-13 15:30:50 +0100

    more tests

--- test_autoarray/structures/triangles/test_area.py
+++ test_autoarray/structures/triangles/test_area.py
@@ -1,2 +1,41 @@
+import numpy as np
+import pytest
+
+from autoarray.structures.triangles.array import ArrayTriangles
+
+
 def test_area(triangles):
     assert triangles.area == 1.0
+
+
+@pytest.mark.parametrize(
+    "vertices, area",
+    [
+        (
+            [
+                [0.0, 0.0],
+                [1.0, 0.0],
+                [0.0, 1.0],
+            ],
+            0.5,
+        ),
+        (
+            [
+                [1.0, 1.0],
+                [1.0, 0.0],
+                [0.0, 1.0],
+            ],
+            0.5,
+        ),
+    ],
+)
+def test_single_triangle(vertices, area):
+    triangles = ArrayTriangles(
+        indices=np.array(
+            [
+                [0, 1, 2],
+            ]
+        ),
+        vertices=np.array(vertices),
+    )
+    assert triangles.area == area
