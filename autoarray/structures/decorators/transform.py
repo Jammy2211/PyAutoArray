478317a2b38b34485d48dd801b1111ff55e399bb
CKrawczyk <coleman.krawczyk@gmail.com>
2024-10-03 10:34:12 +0100

    Small update

--- autoarray/structures/decorators/transform.py
+++ autoarray/structures/decorators/transform.py
@@ -50,7 +50,7 @@ def transform(func):
         """
 
         if not kwargs.get("is_transformed"):
-            kwargs = {"is_transformed": True}
+            kwargs["is_transformed"] = True
 
             transformed_grid = obj.transformed_to_reference_frame_grid_from(
                 grid, **kwargs
