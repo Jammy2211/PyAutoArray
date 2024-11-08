05ebf254c6ade7405b90ea066202ecbbefbe8ab4
Richard <richard@rghsoftware.co.uk>
2024-11-08 11:04:48 +0000

    extracted full routine

--- test_autoarray/structures/triangles/test_coordinate_jax.py
+++ test_autoarray/structures/triangles/test_coordinate_jax.py
@@ -13,15 +13,25 @@ def one_triangle():
     )
 
 
-def test_full_routine(one_triangle, plot):
-    neighborhood = one_triangle.neighborhood()
-    up_sampled = one_triangle.up_sample()
+def full_routine(triangles):
+    neighborhood = triangles.neighborhood()
+    up_sampled = neighborhood.up_sample()
     with_vertices = up_sampled.with_vertices(up_sampled.vertices)
     indexes = with_vertices.containing_indices(Point(0.1, 0.1))
-    selected = up_sampled.for_indexes(indexes)
+    return up_sampled.for_indexes(indexes)
 
-    plot(one_triangle.triangles, color="black")
-    plot(neighborhood.triangles, color="red")
+
+def test_full_routine(one_triangle, plot):
+    full_routine(one_triangle)
+
+    neighborhood = one_triangle.neighborhood()
+    up_sampled = neighborhood.up_sample()
+    with_vertices = up_sampled.with_vertices(up_sampled.vertices)
+
+    selected = full_routine(one_triangle)
+
     plot(up_sampled.triangles, color="blue")
     plot(with_vertices.triangles, color="green")
+    plot(neighborhood.triangles, color="red")
+    plot(one_triangle.triangles, color="black")
     plot(selected.triangles, color="yellow")
