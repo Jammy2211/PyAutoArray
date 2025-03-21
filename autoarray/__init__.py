77d8ec74cce4385c6ffa9eb86c15d93fede7dde1
James Nightingale <james.w.nightingale@durham.ac.uk>
2025-03-21 13:46:58 +0000

    main __init__ import

--- autoarray/__init__.py
+++ autoarray/__init__.py
@@ -89,7 +89,7 @@ from .structures.visibilities import Visibilities
 from .structures.visibilities import VisibilitiesNoiseMap
 
 from autoconf import conf
-from autoconf.fitsable import ndarray_via_hdu_from
+from autoconf.fitsable import ndarray_via_hdu_from, ndarray_via_fits_from
 
 conf.instance.register(__file__)
 
