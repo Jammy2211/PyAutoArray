802e720411102c1d90a1ebab8473f686f3c262cd
Jammy2211 <JNightingale2211@gmail.com>
2025-11-08 12:31:17 +0000

    check

--- test_autoarray/inversion/inversion/test_factory.py
+++ test_autoarray/inversion/inversion/test_factory.py
@@ -114,7 +114,6 @@ def test__inversion_imaging__via_mapper(
     assert inversion.log_det_curvature_reg_matrix_term == pytest.approx(10.6674, 1.0e-4)
     assert inversion.mapped_reconstructed_image == pytest.approx(np.ones(9), 1.0e-4)
 
-
 def test__inversion_imaging__via_regularizations(
     masked_imaging_7x7_no_blur,
     delaunay_mapper_9_3x3,
