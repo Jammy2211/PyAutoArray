795989a661f31af8714a08c604c10ae136e68afb
Jammy2211 <JNightingale2211@gmail.com>
2025-12-09 19:17:45 +0000

    test__voronoi_pixel_areas_for_split

--- test_autoarray/structures/mesh/test_delaunay.py
+++ test_autoarray/structures/mesh/test_delaunay.py
@@ -26,40 +26,6 @@ def test__edge_pixel_list():
     assert mesh.edge_pixel_list == [0, 1, 2, 3, 5, 6, 7, 8]
 
 
-def test__mesh_areas():
-    grid = np.array(
-        [
-            [-2.0, 0.0],
-            [-np.sqrt(2), np.sqrt(2)],
-            [0.0, 0.0],
-            [0.0, 2.0],
-            [np.sqrt(2), np.sqrt(2)],
-            [2.0, 0.0],
-            [np.sqrt(2), -np.sqrt(2)],
-            [0.0, -2.0],
-            [-np.sqrt(2), -np.sqrt(2)],
-        ]
-    )
-
-    mesh = aa.Mesh2DVoronoi(values=grid)
-
-    assert mesh.voronoi_pixel_areas_for_split == pytest.approx(
-        np.array(
-            [
-                -0.1372583,
-                -0.1372583,
-                -0.1372583,
-                -0.1372583,
-                -0.1372583,
-                -0.1372583,
-                -0.1372583,
-                -0.1372583,
-                -0.1372583,
-            ]
-        ),
-        1e-6,
-    )
-
 
 def test__interpolated_array_from():
     grid = aa.Grid2D.no_mask(
@@ -99,17 +65,49 @@ def test__interpolated_array_from():
 
 def test__voronoi_areas_via_delaunay_from():
 
-    import scipy.spatial
-
     mesh_grid = np.array(
         [[0.0, 0.0], [1.1, 0.6], [2.1, 0.1], [0.4, 1.1], [1.1, 7.1], [2.1, 1.1]]
     )
 
-    delaunay = aa.Mesh2DDelaunay(mesh_grid)
+    mesh = aa.Mesh2DDelaunay(mesh_grid)
 
-    voronoi_areas = delaunay.delaunay.voronoi_areas
-
+    voronoi_areas = mesh.delaunay.voronoi_areas
 
     assert voronoi_areas[1] == pytest.approx(1.39137102, 1.0e-4)
     assert voronoi_areas[3] == pytest.approx(29.836324, 1.0e-4)
     assert voronoi_areas[4] == pytest.approx(-1.0, 1.0e-4)
+
+def test__voronoi_pixel_areas_for_split():
+
+    grid = np.array(
+        [
+            [-2.0, 0.0],
+            [-np.sqrt(2), np.sqrt(2)],
+            [0.0, 0.0],
+            [0.0, 2.0],
+            [np.sqrt(2), np.sqrt(2)],
+            [2.0, 0.0],
+            [np.sqrt(2), -np.sqrt(2)],
+            [0.0, -2.0],
+            [-np.sqrt(2), -np.sqrt(2)],
+        ]
+    )
+
+    mesh = aa.Mesh2DDelaunay(grid)
+
+    assert mesh.voronoi_pixel_areas_for_split == pytest.approx(
+        np.array(
+            [
+                -0.1372583,
+                -0.1372583,
+                -0.1372583,
+                -0.1372583,
+                -0.1372583,
+                -0.1372583,
+                -0.1372583,
+                -0.1372583,
+                -0.1372583,
+            ]
+        ),
+        1e-6,
+    )
