9f9cf1e8257775ff9acb421842428574592f9e8a
Jammy2211 <JNightingale2211@gmail.com>
2025-12-12 19:42:02 +0000

    constant split xp fix

--- autoarray/inversion/regularization/constant_split.py
+++ autoarray/inversion/regularization/constant_split.py
@@ -70,7 +70,7 @@ class ConstantSplit(Constant):
         )
 
         pixels = int(len(splitted_mappings) / 4)
-        regularization_weights = np.full(fill_value=self.coefficient, shape=(pixels,))
+        regularization_weights = xp.full(fill_value=self.coefficient, shape=(pixels,))
 
         return regularization_util.pixel_splitted_regularization_matrix_from(
             regularization_weights=regularization_weights,
