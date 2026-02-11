ce674bed3a047558489d7f110eddf8b31d452df5
Jammy2211 <JNightingale2211@gmail.com>
2026-02-11 13:22:16 +0000

    another move

--- autoarray/inversion/pixelization/border_relocator.py
+++ autoarray/inversion/pixelization/border_relocator.py
@@ -284,8 +284,6 @@ def relocated_grid_via_ellipse_border_from(grid, origin, a, b, phi, xp=np, eps=1
         Numerical safety epsilon.
     """
 
-    return grid
-
     # shift to origin
     dy = grid[:, 0] - origin[0]
     dx = grid[:, 1] - origin[1]
@@ -312,6 +310,8 @@ def relocated_grid_via_ellipse_border_from(grid, origin, a, b, phi, xp=np, eps=1
     dy2 = s * xprime2 + c * yprime2
 
     moved = xp.stack([origin[0] + dy2, origin[1] + dx2], axis=1)
+
+    return grid
 
     return xp.where(outside[:, None], moved, grid)
 
